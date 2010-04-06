6c668b8488b8e3235c44dacf5af43a436de80a22
rafal <rafal@9fc1c11e-11b2-4e14-94d8-6c5e05775969>
2010-04-06 22:40:55 +0000

    MERGE: -c 42917 /trunk/Sapphire -> /trunk/Platform
    lsass:rpccli: Copy librpc/netlogon files to lsass.
    
    
    git-svn-id: svn+ssh://source-control.beyondtrust.com/sources/repo/bt-dev/trunk/Platform/src/linux@42983 9fc1c11e-11b2-4e14-94d8-6c5e05775969

---
+++ lsass/client/rpc/netlogon/dsr_enumeratedomaintrusts.c
@@ -0,0 +1,123 @@
+/* Editor Settings: expandtabs and use 4 spaces for indentation
+ * ex: set softtabstop=4 tabstop=8 expandtab shiftwidth=4: *
+ */
+
+/*
+ * Copyright Likewise Software    2004-2008
+ * All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the license, or (at
+ * your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser
+ * General Public License for more details.  You should have received a copy
+ * of the GNU Lesser General Public License along with this program.  If
+ * not, see <http://www.gnu.org/licenses/>.
+ *
+ * LIKEWISE SOFTWARE MAKES THIS SOFTWARE AVAILABLE UNDER OTHER LICENSING
+ * TERMS AS WELL.  IF YOU HAVE ENTERED INTO A SEPARATE LICENSE AGREEMENT
+ * WITH LIKEWISE SOFTWARE, THEN YOU MAY ELECT TO USE THE SOFTWARE UNDER THE
+ * TERMS OF THAT SOFTWARE LICENSE AGREEMENT INSTEAD OF THE TERMS OF THE GNU
+ * LESSER GENERAL PUBLIC LICENSE, NOTWITHSTANDING THE ABOVE NOTICE.  IF YOU
+ * HAVE QUESTIONS, OR WISH TO REQUEST A COPY OF THE ALTERNATE LICENSING
+ * TERMS OFFERED BY LIKEWISE SOFTWARE, PLEASE CONTACT LIKEWISE SOFTWARE AT
+ * license@likewisesoftware.com
+ */
+
+#include "includes.h"
+
+
+WINERR
+DsrEnumerateDomainTrusts(
+    IN  handle_t           hNetrBinding,
+    IN  PCWSTR             pwszServer,
+    IN  UINT32             Flags,
+    OUT NetrDomainTrust  **ppTrusts,
+    OUT PUINT32            pCount
+    )
+{
+    NTSTATUS ntStatus = STATUS_SUCCESS;
+    WINERR dwError = ERROR_SUCCESS;
+    PWSTR pwszServerName = NULL;
+    NetrDomainTrustList TrustList = {0};
+    NetrDomainTrust *pTrusts = NULL;
+    DWORD dwOffset = 0;
+    DWORD dwSpaceLeft = 0;
+    DWORD dwSize = 0;
+
+    BAIL_ON_INVALID_PTR(hNetrBinding, ntStatus);
+    BAIL_ON_INVALID_PTR(pwszServer, ntStatus);
+    BAIL_ON_INVALID_PTR(ppTrusts, ntStatus);
+    BAIL_ON_INVALID_PTR(pCount, ntStatus);
+
+    dwError = LwAllocateWc16String(&pwszServerName, pwszServer);
+    BAIL_ON_WIN_ERROR(dwError);
+
+    DCERPC_CALL(ntStatus, _DsrEnumerateDomainTrusts(hNetrBinding,
+                                                    pwszServerName,
+                                                    Flags,
+                                                    &TrustList));
+    BAIL_ON_NT_STATUS(ntStatus);
+
+    ntStatus = NetrAllocateDomainTrusts(NULL,
+                                        &dwOffset,
+                                        NULL,
+                                        &TrustList,
+                                        &dwSize);
+    BAIL_ON_NT_STATUS(ntStatus);
+
+    dwSpaceLeft = dwSize;
+    dwSize      = 0;
+    dwOffset    = 0;
+
+    ntStatus = NetrAllocateMemory(OUT_PPVOID(&pTrusts),
+                                  dwSpaceLeft);
+    BAIL_ON_NT_STATUS(ntStatus);
+
+    ntStatus = NetrAllocateDomainTrusts(pTrusts,
+                                        &dwOffset,
+                                        &dwSpaceLeft,
+                                        &TrustList,
+                                        &dwSize);
+    BAIL_ON_NT_STATUS(ntStatus);
+
+    *pCount   = TrustList.count;
+    *ppTrusts = pTrusts;
+
+cleanup:
+    NetrCleanStubDomainTrustList(&TrustList);
+    LW_SAFE_FREE_MEMORY(pwszServerName);
+
+    if (dwError == ERROR_SUCCESS &&
+        ntStatus != STATUS_SUCCESS)
+    {
+        dwError = NtStatusToWin32Error(ntStatus);
+    }
+
+    return dwError;
+
+error:
+    if (pTrusts)
+    {
+        NetrFreeMemory(pTrusts);
+    }
+
+    *pCount   = 0;
+    *ppTrusts = NULL;
+
+    goto cleanup;
+}
+
+
+/*
+local variables:
+mode: c
+c-basic-offset: 4
+indent-tabs-mode: nil
+tab-width: 4
+end:
+*/
