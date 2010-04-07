3e312074b0764a1ed5e28496373ad25f99ec0155
rafal <rafal@9fc1c11e-11b2-4e14-94d8-6c5e05775969>
2010-04-07 23:46:44 +0000

    lsass:rpccli: Copy libkrb5pac from librpc.
    git-svn-id: svn+ssh://source-control.beyondtrust.com/sources/repo/bt-dev/trunk/Platform/src/linux@43053 9fc1c11e-11b2-4e14-94d8-6c5e05775969

---
+++ lsass/client/rpc/krb5pac/krb5pac_stub.h
@@ -0,0 +1,23 @@
+/* This file is a copy of excerpt from generated krb5pac_h.h header.
+   It has to be in sync with original idl file.
+ */
+
+#ifndef _KRB5PAC_STUB_H_
+#define _KRB5PAC_STUB_H_
+
+
+extern void PAC_LOGON_INFO_Encode(
+#ifdef IDL_PROTOTYPES
+    /* [in] */ idl_es_handle_t h,
+    /* [in] */ PAC_LOGON_INFO *logon_info
+#endif
+);
+extern void PAC_LOGON_INFO_Decode(
+#ifdef IDL_PROTOTYPES
+    /* [in] */ idl_es_handle_t h,
+    /* [out] */ PAC_LOGON_INFO **logon_info
+#endif
+);
+
+
+#endif /* _KRB5PAC_STUB_H_ */
