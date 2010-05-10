33d5761788d39b30b1185160af082f44f5a4d14f
jerry <jerry@9fc1c11e-11b2-4e14-94d8-6c5e05775969>
2010-05-10 21:50:50 +0000

    srv: Add SRV_ECP_TYPE_ABE definition
    git-svn-id: svn+ssh://source-control.beyondtrust.com/sources/repo/bt-dev/trunk/Platform/src/linux@43982 9fc1c11e-11b2-4e14-94d8-6c5e05775969

--- lwio/server/include/srvecp.h
+++ lwio/server/include/srvecp.h
@@ -33,5 +33,6 @@
 #define SRV_ECP_TYPE_SHARE_NAME     "Likewise.SRV.ShareName"
 #define SRV_ECP_TYPE_CLIENT_ADDRESS "Likewise.SRV.ClientAddress"
 #define SRV_ECP_TYPE_MAX_ACCESS     "Likewise.SRV.MaximalAccess"
+#define SRV_ECP_TYPE_ABE            "Likewise.SRV.AccessBasedEnumeration"
 
 #endif /* __SRV_ECP_H__ */
