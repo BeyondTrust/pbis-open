aac2d5d93a9182be942049bc964f026106ac8cce
snambakam <snambakam@9fc1c11e-11b2-4e14-94d8-6c5e05775969>
2010-05-25 21:56:38 +0000

    lwio:srv: don't log parameters as part of stats. Also, provide API to set SessionInfo.
    git-svn-id: svn+ssh://source-control.beyondtrust.com/sources/repo/bt-dev/trunk/Platform/src/linux@44940 9fc1c11e-11b2-4e14-94d8-6c5e05775969

--- lwio/include/lwio/lwiosrvstatprovider.h
+++ lwio/include/lwio/lwiosrvstatprovider.h
@@ -71,58 +71,6 @@ typedef struct _SRV_STAT_SESSION_INFO
 
 } SRV_STAT_SESSION_INFO, *PSRV_STAT_SESSION_INFO;
 
-typedef struct _SRV_STAT_TREE_INFO
-{
-    PWSTR pwszShareName;
-    ULONG ulTreeId;
-
-} SRV_STAT_TREE_INFO, *PSRV_STAT_TREE_INFO;
-
-typedef struct _SRV_STAT_FILE_INFO
-{
-    PWSTR   pwszFileName;
-    ULONG64 ullVolatileFileId;
-    ULONG64 ullPersistentFileId;
-
-} SRV_STAT_FILE_INFO, *PSRV_STAT_FILE_INFO;
-
-typedef struct _SRV_STAT_CREATE_PARAMS_SMB_V1
-{
-
-} SRV_STAT_CREATE_PARAMS_SMB_V1, *PSRV_STAT_CREATE_PARAMS_SMB_V1;
-
-typedef struct _SRV_STAT_CREATE_PARAMS_SMB_V2
-{
-
-} SRV_STAT_CREATE_PARAMS_SMB_V2, *PSRV_STAT_CREATE_PARAMS_SMB_V2;
-
-typedef struct _SRV_STAT_CLOSE_PARAMS_SMB_V1
-{
-
-} SRV_STAT_CLOSE_PARAMS_SMB_V1, *PSRV_STAT_CLOSE_PARAMS_SMB_V1;
-
-typedef struct _SRV_STAT_CLOSE_PARAMS_SMB_V2
-{
-
-} SRV_STAT_CLOSE_PARAMS_SMB_V2, *PSRV_STAT_CLOSE_PARAMS_SMB_V2;
-
-typedef struct _SRV_STAT_REQUEST_PARAMETERS
-{
-    PSRV_STAT_SESSION_INFO    pSessionInfo;
-    PSRV_STAT_TREE_INFO       pTreeInfo;
-
-    ULONG                     ulOpCode;
-    union
-    {
-        PSRV_STAT_CREATE_PARAMS_SMB_V1 pCreateParams_SMB_V1;
-        PSRV_STAT_CREATE_PARAMS_SMB_V2 pCreateParams_SMB_V2;
-
-        PSRV_STAT_CLOSE_PARAMS_SMB_V1  pCloseParams_SMB_V1;
-        PSRV_STAT_CLOSE_PARAMS_SMB_V2  pCloseParams_SMB_V2;
-    };
-
-} SRV_STAT_REQUEST_PARAMETERS, *PSRV_STAT_REQUEST_PARAMETERS;
-
 typedef NTSTATUS
         (*PFN_SRV_STAT_CREATE_REQUEST_CONTEXT)(
             PSRV_STAT_CONNECTION_INFO  pConnection,        /* IN              */
@@ -135,60 +83,26 @@ typedef NTSTATUS
         (*PFN_SRV_STAT_PUSH_MESSAGE)(
             HANDLE                       hContext,         /* IN              */
             ULONG                        ulOpcode,         /* IN              */
-            PSRV_STAT_REQUEST_PARAMETERS pParams,          /* IN     OPTIONAL */
             PBYTE                        pMessage,         /* IN     OPTIONAL */
             ULONG                        ulMessageLen      /* IN              */
             );
 
 typedef NTSTATUS
+        (*PFN_SRV_STAT_SET_SESSION_INFO)(
+            HANDLE                    hContext,            /* IN              */
+            PSRV_STAT_SESSION_INFO    pSessionInfo         /* IN              */
+            );
+
+typedef NTSTATUS
         (*PFN_SRV_STAT_SET_SUB_OP_CODE)(
             HANDLE                     hContext,           /* IN              */
-            ULONG                     ulSubOpcode          /* IN              */
+            ULONG                      ulSubOpcode         /* IN              */
             );
 
 typedef NTSTATUS
         (*PFN_SRV_STAT_SET_IOCTL)(
             HANDLE                    hContext,            /* IN              */
             ULONG                     ulIoCtlCode          /* IN              */
-            );
-
-typedef NTSTATUS
-        (*PFN_SRV_STAT_SESSION_CREATED)(
-            HANDLE                    hContext,            /* IN              */
-            PSRV_STAT_SESSION_INFO    pSessionInfo         /* IN              */
-            );
-
-typedef NTSTATUS
-        (*PFN_SRV_STAT_TREE_CREATED)(
-            HANDLE                    hContext,            /* IN              */
-            PSRV_STAT_SESSION_INFO    pSessionInfo,        /* IN              */
-            PSRV_STAT_TREE_INFO       pTreeInfo            /* IN              */
-            );
-
-typedef NTSTATUS
-        (*PFN_SRV_STAT_FILE_CREATED)(
-            HANDLE                    hContext,            /* IN              */
-            PSRV_STAT_SESSION_INFO    pSessionInfo,        /* IN              */
-            PSRV_STAT_TREE_INFO       pTreeInfo,           /* IN              */
-            PSRV_STAT_FILE_INFO       pFileInfo            /* IN              */
-            );
-
-typedef NTSTATUS
-        (*PFN_SRV_STAT_FILE_CLOSED)(
-            HANDLE                    hContext,            /* IN              */
-            PSRV_STAT_FILE_INFO       pFileInfo            /* IN              */
-            );
-
-typedef NTSTATUS
-        (*PFN_SRV_STAT_TREE_CLOSED)(
-            HANDLE                     hContext,           /* IN              */
-            PSRV_STAT_TREE_INFO       pTreeInfo            /* IN              */
-            );
-
-typedef NTSTATUS
-        (*PFN_SRV_STAT_SESSION_CLOSED)(
-            HANDLE                    hContext,            /* IN              */
-            PSRV_STAT_SESSION_INFO    pSessionInfo         /* IN              */
             );
 
 typedef NTSTATUS
@@ -217,12 +131,7 @@ typedef struct _LWIO_SRV_STAT_PROVIDER_FUNCTION_TABLE
     PFN_SRV_STAT_PUSH_MESSAGE           pfnPushMessage;
     PFN_SRV_STAT_SET_SUB_OP_CODE        pfnSetSubOpCode;
     PFN_SRV_STAT_SET_IOCTL              pfnSetIOCTL;
-    PFN_SRV_STAT_SESSION_CREATED        pfnSessionCreated;
-    PFN_SRV_STAT_TREE_CREATED           pfnTreeCreated;
-    PFN_SRV_STAT_FILE_CREATED           pfnFileCreated;
-    PFN_SRV_STAT_FILE_CLOSED            pfnFileClosed;
-    PFN_SRV_STAT_TREE_CLOSED            pfnTreeClosed;
-    PFN_SRV_STAT_SESSION_CLOSED         pfnSessionClosed;
+    PFN_SRV_STAT_SET_SESSION_INFO       pfnSetSessionInfo;
     PFN_SRV_STAT_POP_MESSAGE            pfnPopMessage;
     PFN_SRV_STAT_SET_RESPONSE_INFO      pfnSetResponseInfo;
     PFN_SRV_STAT_CLOSE_REQUEST_CONTEXT  pfnCloseRequestContext;
