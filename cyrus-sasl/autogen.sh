74b036d5147e9f10219039b6762ce746e3a57117
CORP\aberry <CORP\aberry@9fc1c11e-11b2-4e14-94d8-6c5e05775969>
2010-07-15 21:59:53 +0000

    Remove a couple more generated files, add a component file, make a couple of other changes so that cyrus-sasl can be built.
    git-svn-id: svn+ssh://source-control.beyondtrust.com/sources/repo/bt-dev/trunk/Platform/src/linux@47439 9fc1c11e-11b2-4e14-94d8-6c5e05775969

--- cyrus-sasl/autogen.sh
+++ cyrus-sasl/autogen.sh
@@ -3,16 +3,16 @@
 # From SMakefile in Cyrus SASL source repository
 
 rm -f aclocal.m4
-libtoolize -c -f || exit $?
-aclocal -I cmulocal -I config || exit $?
-autoheader || exit $?
-autoconf || exit $?
+libtoolize -fic || exit $?
+aclocal --force -I cmulocal -I config || exit $?
+autoheader -f || exit $?
+autoconf -f || exit $?
 automake --add-missing --include-deps || exit $?
 
 cd saslauthd || exit $?
 rm -f aclocal.m4
-libtoolize -c -f || exit $?
-aclocal -I ../cmulocal -I ../config || exit $?
-autoheader || exit $?
-autoconf || exit $?
+libtoolize -fic || exit $?
+aclocal --force -I ../cmulocal -I ../config || exit $?
+autoheader -f || exit $?
+autoconf -f || exit $?
 automake --add-missing --include-deps || exit $?
