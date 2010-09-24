BeyondTrust/pbis-open

commit 67bedf62d5e00fb79969bbea2b8436356850fe39

Reduce global variable usage in the AD provider.
CR:  dalmeida


git-svn-id: svn+ssh://source-control.beyondtrust.com/sources/repo/bt-dev/trunk/Platform/src/linux@50533 9fc1c11e-11b2-4e14-94d8-6c5e05775969

lsass/server/auth-providers/ad-open-provider/lsaum_p.c
@@ -125,6 +125,9 @@ typedef struct _LSA_UM_KSCHEDULES {
 /// Keeps track of all domain state.
 ///
 typedef struct _LSA_UM_STATE {
+    /// Provider state.
+    PLSA_AD_PROVIDER_STATE pProviderState;
+
     /// Linked list of users.
     PLSA_UM_USER_REFRESH_LIST UserList;
 
@@ -207,6 +210,7 @@ static
 DWORD
 LsaUmpRefreshUserCreds(
     LSA_UM_STATE_HANDLE       Handle,
+    PAD_PROVIDER_CONTEXT      pContext,
     PLSA_UM_USER_REFRESH_ITEM pItem
     );
 
@@ -405,6 +409,7 @@ LsaUmpStateDestroy(
 
 DWORD
 LsaUmpStateCreate(
+    IN PLSA_AD_PROVIDER_STATE pProviderState,
     OUT PLSA_UM_STATE_HANDLE pHandle
     )
 ///<
@@ -427,6 +432,8 @@ LsaUmpStateCreate(
                   sizeof(*pState),
                   (PVOID*)&pState);
     BAIL_ON_LSA_ERROR(dwError);
+
+    pState->pProviderState = pProviderState;
 
     pState->dwCheckUsersSeconds = LSA_UM_THREAD_MIN_PERIOD;
 
@@ -642,6 +649,7 @@ LsaUmpCheckUsers(
     PLSA_UM_USER_REFRESH_ITEM pItem = NULL;
     PLSA_UM_USER_REFRESH_ITEM pNextItem = NULL;
     DWORD                     dwTime = 0;
+    PAD_PROVIDER_CONTEXT      pProviderContext = NULL;
 
     LSA_LOG_VERBOSE("Lsa User Manager - checking user credentials refresh list");
 
@@ -692,14 +700,27 @@ LsaUmpCheckUsers(
         }
     }
 
-    ADSyncTimeToDC(gpADProviderData->szDomain);
+    ADSyncTimeToDC(Handle->pProviderState, Handle->pProviderState->pProviderData->szDomain);
 
-    bDomainIsOffline = LsaDmIsDomainOffline(gpADProviderData->szDomain);
-    bShouldRefreshCreds = AD_ShouldRefreshUserCreds();
+    bDomainIsOffline = LsaDmIsDomainOffline(Handle->pProviderState->hDmState, Handle->pProviderState->pProviderData->szDomain);
+    bShouldRefreshCreds = AD_ShouldRefreshUserCreds(Handle->pProviderState);
 
     if ( bDomainIsOffline )
     {
         LSA_LOG_DEBUG("LSA User Manager - domain is offline");
+    }
+
+    if (bShouldRefreshCreds)
+    {
+        dwError = AD_CreateProviderContext(&pProviderContext);
+        if (dwError)
+        {
+            bShouldRefreshCreds = FALSE;
+        }
+        else
+        {
+            pProviderContext->pState = Handle->pProviderState;
+        }
     }
 
     if ( pUserList )
@@ -737,12 +758,14 @@ LsaUmpCheckUsers(
                 // ignore errors because we want to process all users
                 LsaUmpRefreshUserCreds(
                     Handle,
+                    pProviderContext,
                     pItem);
             }
         }
     }
 
     LsaUmpFreeRequestList(pRequestList);
+    AD_DereferenceProviderContext(pProviderContext);
 
     return dwError;
 }
@@ -1488,6 +1511,7 @@ static
 DWORD
 LsaUmpRefreshUserCreds(
     LSA_UM_STATE_HANDLE       Handle,
+    PAD_PROVIDER_CONTEXT      pProviderContext,
     PLSA_UM_USER_REFRESH_ITEM pUserItem
     )
 {
@@ -1523,7 +1547,7 @@ LsaUmpRefreshUserCreds(
         NULL);
 
     dwError = AD_OnlineCheckUserPassword(
-                    (HANDLE)NULL,
+                    pProviderContext,
                     pUserInfo,
                     pszPassword,
                     &pUserItem->dwTgtEndTime);
@@ -1535,7 +1559,7 @@ LsaUmpRefreshUserCreds(
     // post-processing fails.
     pUserItem->dwFailedCount = 0;
 
-    if (AD_EventlogEnabled())
+    if (AD_EventlogEnabled(Handle->pProviderState))
     {
         LsaUmpLogUserTGTRefreshSuccessEvent(pUserInfo->userInfo.pszUPN,
                                             pUserItem->uUid,
@@ -1556,7 +1580,7 @@ error:
     {
         pUserItem->dwFailedCount++;
 
-        if (AD_EventlogEnabled())
+        if (AD_EventlogEnabled(Handle->pProviderState))
         {
             LsaUmpLogUserTGTRefreshFailureEvent(pUserInfo->userInfo.pszUPN,
                                                 pUserItem->uUid,

lsass/server/auth-providers/ad-open-provider/Makefile.am
@@ -45,7 +45,6 @@ liblsass_auth_provider_ad_open_la_SOURCES = \
        provider-main.c           \
        providerstate.c           \
        lsadm.c                   \
-       lsadm_p.c                 \
        lsadmengine.c             \
        lsadmwrap.c               \
        media-sense.c             \

lsass/server/auth-providers/ad-open-provider/ad_marshal_group.c
@@ -52,14 +52,15 @@
 
 DWORD
 ADMarshalGetCanonicalName(
-    PLSA_SECURITY_OBJECT     pObject,
-    PSTR*                   ppszResult)
+    PLSA_AD_PROVIDER_STATE pState,
+    PLSA_SECURITY_OBJECT   pObject,
+    PSTR*                  ppszResult)
 {
     DWORD dwError = LW_ERROR_SUCCESS;
     PSTR    pszResult = NULL;
     PSTR pszDefaultPrefix = NULL;
 
-    dwError = AD_GetUserDomainPrefix(&pszDefaultPrefix);
+    dwError = AD_GetUserDomainPrefix(pState, &pszDefaultPrefix);
     BAIL_ON_LSA_ERROR(dwError);
 
     if(pObject->type == LSA_OBJECT_TYPE_GROUP &&
@@ -88,7 +89,7 @@ ADMarshalGetCanonicalName(
             ' ',
             LsaSrvSpaceReplacement());
     }
-    else if (AD_ShouldAssumeDefaultDomain() &&
+    else if (AD_ShouldAssumeDefaultDomain(pState) &&
         pObject->enabled &&
         !strcmp(pObject->pszNetbiosDomainName, pszDefaultPrefix))
     {

lsass/server/auth-providers/ad-open-provider/ad_marshal_group.h
@@ -49,8 +49,9 @@
 
 DWORD
 ADMarshalGetCanonicalName(
-    PLSA_SECURITY_OBJECT     pObject,
-    PSTR*                   ppszResult);
+    PLSA_AD_PROVIDER_STATE pState,
+    PLSA_SECURITY_OBJECT   pObject,
+    PSTR*                  ppszResult);
 
 #endif //__LSALDAP_MARSHAL_GROUP_H__
 

lsass/server/auth-providers/ad-open-provider/ad_marshal_user.c
@@ -181,6 +181,7 @@ ADNonSchemaKeywordGetUInt32(
 
 DWORD
 AD_BuildHomeDirFromTemplate(
+    PLSA_AD_PROVIDER_STATE pState,
     PCSTR pszHomedirTemplate,
     PCSTR pszNetBIOSDomainName,
     PCSTR pszSamAccountName,
@@ -207,7 +208,7 @@ AD_BuildHomeDirFromTemplate(
 
     if (strstr(pszHomedirTemplate, "%H"))
     {
-        dwError = AD_GetHomedirPrefixPath(&pszHomedirPrefix);
+        dwError = AD_GetHomedirPrefixPath(pState, &pszHomedirPrefix);
         BAIL_ON_LSA_ERROR(dwError);
 
         BAIL_ON_INVALID_STRING(pszHomedirPrefix);

lsass/server/auth-providers/ad-open-provider/ad_marshal_user_p.h
@@ -83,6 +83,7 @@ ADNonSchemaKeywordGetUInt32(
 
 DWORD
 AD_BuildHomeDirFromTemplate(
+    PLSA_AD_PROVIDER_STATE pState,
     PCSTR pszHomedirTemplate,
     PCSTR pszNetBIOSDomainName,
     PCSTR pszSamAccountName,

lsass/server/auth-providers/ad-open-provider/adcache.c
@@ -48,6 +48,7 @@
 DWORD
 ADCacheOpen(
     IN PCSTR pszDbPath,
+    IN PLSA_AD_PROVIDER_STATE pState,
     OUT PLSA_DB_HANDLE phDb
     )
 {
@@ -55,6 +56,7 @@ ADCacheOpen(
 
     dwError = (*gpCacheProvider->pfnOpenHandle)(
                         pszDbPath,
+                        pState,
                         phDb
                         );
     return dwError;

lsass/server/auth-providers/ad-open-provider/adcache.h
@@ -48,10 +48,6 @@
 #ifndef __ADCACHE_H__
 #define __ADCACHE_H__
 
-struct _LSA_DB_CONNECTION;
-typedef struct _LSA_DB_CONNECTION *LSA_DB_HANDLE;
-typedef LSA_DB_HANDLE *PLSA_DB_HANDLE;
-
 // The bIsInLdap, bIsInPac, and bIsInPacOnly fields indicate where the
 // membership came from. Only the following combinations are valid:
 //
@@ -144,6 +140,7 @@ typedef struct __LSA_DB_PASSWORD_VERIFIER
 DWORD
 ADCacheOpen(
     IN PCSTR pszDbPath,
+    IN PLSA_AD_PROVIDER_STATE pState,
     PLSA_DB_HANDLE phDb
     );
 

commit 40c641246af174c7f1a2a03ad7d46f051b11cb6f

Fix a segfault which was caused by r50533.
CR:  wfu


git-svn-id: svn+ssh://source-control.beyondtrust.com/sources/repo/bt-dev/trunk/Platform/src/linux@50733 9fc1c11e-11b2-4e14-94d8-6c5e05775969

lsass/server/auth-providers/ad-open-provider/lsaum_p.c
@@ -1535,7 +1535,7 @@ LsaUmpRefreshUserCreds(
     }
 
     dwError = AD_FindUserObjectById(
-                  (HANDLE)NULL,
+                  pProviderContext,
                   pUserItem->uUid,
                   &pUserInfo);
     BAIL_ON_LSA_ERROR(dwError);