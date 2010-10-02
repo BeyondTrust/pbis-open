bdc9bbb8d0cff2b69260246f9063571481774e6e
bkoropoff <bkoropoff@9fc1c11e-11b2-4e14-94d8-6c5e05775969>
2010-10-02 00:24:17 +0000

    Add master header file for lwadvapi for convenience
    git-svn-id: svn+ssh://source-control.beyondtrust.com/sources/repo/bt-dev/trunk/Platform/src/linux@51032 9fc1c11e-11b2-4e14-94d8-6c5e05775969

---
+++ lwadvapi/include/public/lwadvapi.h
@@ -0,0 +1,14 @@
+#include <lwdef.h>
+#include <lwerror.h>
+#include <lwlogging.h>
+#include <lwfile.h>
+#include <lwkrb5.h>
+#include <lwldap.h>
+#include <lwmem.h>
+#include <lwsecurityidentifier.h>
+#include <lwstr.h>
+#include <lwtime.h>
+#include <lwsid.h>
+#include <lwldap-error.h>
+#include <lwdscache.h>
+#include <lwbuffer.h>
