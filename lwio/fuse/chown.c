4e6323c78dc01ca00fdc548aa94e60fddff622e6
bkoropoff <bkoropoff@9fc1c11e-11b2-4e14-94d8-6c5e05775969>
2010-10-06 20:28:13 +0000

    Properly implement chown()
    git-svn-id: svn+ssh://source-control.beyondtrust.com/sources/repo/bt-dev/trunk/Platform/src/linux@51167 9fc1c11e-11b2-4e14-94d8-6c5e05775969

--- lwio/fuse/chown.c
+++ lwio/fuse/chown.c
@@ -37,6 +37,123 @@ LwIoFuseChown(
     gid_t gid
     )
 {
-    /* Currently a no-op */
-    return STATUS_SUCCESS;
+    NTSTATUS status = STATUS_SUCCESS;
+    PLW_MAP_SECURITY_CONTEXT pContext = NULL;
+    PSID pUserSid = NULL;
+    PSID pGroupSid = NULL;
+    PSECURITY_DESCRIPTOR_ABSOLUTE pAbsolute = NULL;
+    PSECURITY_DESCRIPTOR_RELATIVE pRelative = NULL;
+    ULONG ulLength = 0;
+    IO_STATUS_BLOCK ioStatus = {0};
+    IO_FILE_HANDLE handle = NULL;
+    PIO_FUSE_CONTEXT pFuseContext = NULL;
+    IO_FILE_NAME filename = {0};
+
+    status = LwMapSecurityCreateContext(&pContext);
+    BAIL_ON_NT_STATUS(status);
+
+    if (uid != (uid_t) -1)
+    {
+        status = LwMapSecurityGetSidFromId(pContext, &pUserSid, TRUE, (ULONG) uid);
+        BAIL_ON_NT_STATUS(status);
+    }
+
+    if (gid != (gid_t) -1)
+    {
+        status = LwMapSecurityGetSidFromId(pContext, &pGroupSid, FALSE, (ULONG) gid);
+        BAIL_ON_NT_STATUS(status);
+    }
+
+    status = RTL_ALLOCATE(&pAbsolute, VOID, SECURITY_DESCRIPTOR_ABSOLUTE_MIN_SIZE);
+    BAIL_ON_NT_STATUS(status);
+
+    status = RtlCreateSecurityDescriptorAbsolute(pAbsolute, SECURITY_DESCRIPTOR_REVISION);
+    BAIL_ON_NT_STATUS(status);
+
+    if (pUserSid)
+    {
+        status = RtlSetOwnerSecurityDescriptor(pAbsolute, pUserSid, FALSE);
+        BAIL_ON_NT_STATUS(status);
+    }
+
+    if (pGroupSid)
+    {
+        status = RtlSetGroupSecurityDescriptor(pAbsolute, pGroupSid, FALSE);
+        BAIL_ON_NT_STATUS(status);
+    }
+
+    status = RtlAbsoluteToSelfRelativeSD(
+        pAbsolute,
+        NULL,
+        &ulLength);
+    if (status != STATUS_BUFFER_TOO_SMALL)
+    {
+        BAIL_ON_NT_STATUS(status);
+    }
+    status = STATUS_SUCCESS;
+
+    status = RTL_ALLOCATE(&pRelative, VOID, ulLength);
+    BAIL_ON_NT_STATUS(status);
+
+    status = RtlAbsoluteToSelfRelativeSD(
+        pAbsolute,
+        pRelative,
+        &ulLength);
+    BAIL_ON_NT_STATUS(status);
+
+    pFuseContext = LwIoFuseGetContext();
+
+    status = LwIoFuseGetNtFilename(
+        pFuseContext,
+        pszPath,
+        &filename);
+    BAIL_ON_NT_STATUS(status);
+
+    status = LwNtCreateFile(
+        &handle,                 /* File handle */
+        NULL,                    /* Async control block */
+        &ioStatus,               /* IO status block */
+        &filename,               /* Filename */
+        NULL,                    /* Security descriptor */
+        NULL,                    /* Security QOS */
+        WRITE_OWNER,             /* Desired access mask */
+        0,                       /* Allocation size */
+        0,                       /* File attributes */
+        FILE_SHARE_READ |
+        FILE_SHARE_WRITE |
+        FILE_SHARE_DELETE,       /* Share access */
+        FILE_OPEN,               /* Create disposition */
+        0,                       /* Create options */
+        NULL,                    /* EA buffer */
+        0,                       /* EA length */
+        NULL,                     /* ECP list */
+        NULL);
+    BAIL_ON_NT_STATUS(status);
+
+    status = LwNtSetSecurityFile(
+        handle, /* File handle */
+        NULL, /* Async control block */
+        &ioStatus, /* IO status block */
+        (pUserSid ? OWNER_SECURITY_INFORMATION : 0) |
+        (pGroupSid ? GROUP_SECURITY_INFORMATION : 0),
+        pRelative,
+        ulLength);
+    BAIL_ON_NT_STATUS(status);
+
+error:
+
+    LwMapSecurityFreeContext(&pContext);
+
+    if (handle)
+    {
+        LwNtCloseFile(handle);
+    }
+
+    RTL_FREE(&filename.FileName);
+    RTL_FREE(&pUserSid);
+    RTL_FREE(&pGroupSid);
+    RTL_FREE(&pRelative);
+    RTL_FREE(&pAbsolute);
+
+    return status;
 }
