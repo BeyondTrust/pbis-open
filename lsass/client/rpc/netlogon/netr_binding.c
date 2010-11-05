652cbbdd49d6bde0a77fb4919915c99717a990b1
rafal <rafal@9fc1c11e-11b2-4e14-94d8-6c5e05775969>
2010-11-05 18:09:15 +0000

    lsass:rpccli: Fix typo resulting in dead code.
    Fixes CID: 10664
    
    
    
    git-svn-id: svn+ssh://source-control.beyondtrust.com/sources/repo/bt-dev/trunk/Platform/src/linux@52288 9fc1c11e-11b2-4e14-94d8-6c5e05775969

--- lsass/client/rpc/netlogon/netr_binding.c
+++ lsass/client/rpc/netlogon/netr_binding.c
@@ -166,7 +166,7 @@ NetrInitBindingFull(
     NETR_BINDING hBinding = NULL;
 
     BAIL_ON_INVALID_PTR(phBinding, ntStatus);
-    BAIL_ON_INVALID_PTR(pszProtSeq, ntStatus);
+    BAIL_ON_INVALID_PTR(pwszProtSeq, ntStatus);
 
     dwError = LwWc16sToMbs(pwszProtSeq, &pszProtSeq);
     BAIL_ON_WIN_ERROR(dwError);
