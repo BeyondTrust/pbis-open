4b36eb26bf2c3d5c087b344bf51928e17ff50732
CORP\keithr <CORP\keithr@9fc1c11e-11b2-4e14-94d8-6c5e05775969>
2011-01-12 23:13:06 +0000

    MERGE: -c 53663 /branches/likewise-oem-lexmark -> /trunk/Platform
    Minor cleanups.
    
    
    git-svn-id: svn+ssh://source-control.beyondtrust.com/sources/repo/bt-dev/trunk/Platform/src/linux@54128 9fc1c11e-11b2-4e14-94d8-6c5e05775969

--- lwautoenroll/lib/ssl.c
+++ lwautoenroll/lib/ssl.c
@@ -9,6 +9,7 @@
 #include <lwmem.h>
 #include <lwstr.h>
 
+#include <stdarg.h>
 #include <string.h>
 
 DWORD
@@ -18,20 +19,16 @@ LwSSLErrorToLwError(unsigned long ssl_error)
     {
         case ERR_R_MALLOC_FAILURE:
             return LW_ERROR_OUT_OF_MEMORY;
-            break;
 
         case ERR_R_PASSED_NULL_PARAMETER:
             return LW_ERROR_INVALID_PARAMETER;
-            break;
 
         case ERR_R_SHOULD_NOT_HAVE_BEEN_CALLED:
         case ERR_R_INTERNAL_ERROR:
             return LW_ERROR_INTERNAL;
-            break;
 
         default:
-            return LW_ERROR_INTERNAL; /* XXX Add LW_ERROR_SSL_ERROR */
-            break;
+            return LW_ERROR_UNKNOWN;
     }
 }
 
@@ -243,7 +240,7 @@ BufAppend(
     }
 
     buf->length = new_length;
-    strncpy(buf->data + old_length, value, length);
+    memcpy(buf->data + old_length, value, length);
     buf->data[buf->length] = '\0';
 
 cleanup:
