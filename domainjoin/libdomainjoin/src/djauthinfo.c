BeyondTrust/pbis-open

commit 3ea8029299135897d0935c8e8f3d2f50209eb55b

domainjoin: Stop using LsaNetTestJoinDomain() since it will go away.
git-svn-id: svn+ssh://source-control.beyondtrust.com/sources/repo/bt-dev/trunk/Platform/src/linux@53964 9fc1c11e-11b2-4e14-94d8-6c5e05775969

domainjoin/libdomainjoin/src/djauthinfo.c
@@ -745,9 +745,45 @@ DJTestJoin(
     LWException **exc
     )
 {
-    LW_CLEANUP_LSERR(exc, LsaNetTestJoinDomain(pszDomainName, isValid));
+    DWORD dwError = 0;
+    BOOLEAN isJoined = FALSE;
+    HANDLE hLsaConnection = NULL;
+    PLSA_MACHINE_ACCOUNT_INFO_A pAccountInfo = NULL;
+
+    dwError = LsaOpenServer(&hLsaConnection);
+    LW_CLEANUP_LSERR(exc, dwError);
+
+    dwError = LsaAdGetMachineAccountInfo(hLsaConnection, NULL, &pAccountInfo);
+    switch (dwError)
+    {
+        case NERR_SetupNotJoined:
+            isJoined = FALSE;
+            dwError = 0;
+            break;
+        case 0:
+            isJoined = TRUE;
+            break;
+        default:
+            LW_CLEANUP_LSERR(exc, dwError);
+    }
+
 cleanup:
-    ;
+    if (dwError)
+    {
+        isJoined = FALSE;
+    }
+
+    if (pAccountInfo)
+    {
+        LsaAdFreeMachineAccountInfo(pAccountInfo);
+    }
+
+    if (hLsaConnection)
+    {
+        LsaCloseServer(hLsaConnection);
+    }
+
+    *isValid = isJoined;
 }
 
 const JoinModule DJDoJoinModule = { TRUE, "join", "join computer to AD", QueryDoJoin, DoJoin, GetJoinDescription };

commit 5b604ea180fef89079c0d2e8096ccc248a9afd51

Fix domain leave, broken by r53964.
CR:  dalmeida


git-svn-id: svn+ssh://source-control.beyondtrust.com/sources/repo/bt-dev/trunk/Platform/src/linux@54897 9fc1c11e-11b2-4e14-94d8-6c5e05775969

domainjoin/libdomainjoin/src/djauthinfo.c
@@ -751,7 +751,7 @@ DJTestJoin(
     dwError = LsaOpenServer(&hLsaConnection);
     LW_CLEANUP_LSERR(exc, dwError);
 
-    dwError = LsaAdGetMachineAccountInfo(hLsaConnection, NULL, &pAccountInfo);
+    dwError = LsaAdGetMachineAccountInfo(hLsaConnection, pszDomainName, &pAccountInfo);
     switch (dwError)
     {
         case NERR_SetupNotJoined: