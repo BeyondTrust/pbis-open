BeyondTrust/pbis-open

commit f0ea97d4ea6c3708d9f791a81c6066c6816d5669

lsass:pstore: Cleanup intermediate layer and simplify backend; store kvno; store information info in new format.
CR: aberry@likewise.com

git-svn-id: svn+ssh://source-control.beyondtrust.com/sources/repo/bt-dev/trunk/Platform/src/linux@54873 9fc1c11e-11b2-4e14-94d8-6c5e05775969

lsass/pstore/lsapstore-main.c
@@ -44,26 +44,7 @@
  */
 
 #include "lsapstore-includes.h"
-
-DWORD
-LsaPstoreGetPasswordInfoA(
-    IN OPTIONAL PCSTR DnsDomainName,
-    OUT PLSA_MACHINE_PASSWORD_INFO_A* PasswordInfo
-    )
-{
-    DWORD dwError = 0;
-    int EE = 0;
-
-    dwError = LsaPstorepEnsureInitialized();
-    GOTO_CLEANUP_ON_WINERROR_EE(dwError, EE);
-
-    dwError = LsaPstorepBackendGetPasswordInfoA(DnsDomainName, PasswordInfo);
-    GOTO_CLEANUP_ON_WINERROR_EE(dwError, EE);
-
-cleanup:
-    LSA_PSTORE_LOG_LEAVE_ERROR_EE(dwError, EE);
-    return dwError;
-}
+#include "lsapstore-backend.h"
 
 DWORD
 LsaPstoreGetPasswordInfoW(
@@ -73,33 +54,51 @@ LsaPstoreGetPasswordInfoW(
 {
     DWORD dwError = 0;
     int EE = 0;
+    PLSA_PSTORE_BACKEND_STATE backendState = NULL;
+    PWSTR defaultDnsDomainName = NULL;
+    PCWSTR actualDnsDomainName = DnsDomainName;
+    PLSA_MACHINE_PASSWORD_INFO_W passwordInfo = NULL;
 
-    dwError = LsaPstorepEnsureInitialized();
+    if (DnsDomainName && !LsaPstorepWC16StringIsUpcase(DnsDomainName))
+    {
+        dwError = ERROR_INVALID_PARAMETER;
+        GOTO_CLEANUP_EE(EE);
+    }
+
+    dwError = LsaPstorepEnsureInitialized(&backendState);
     GOTO_CLEANUP_ON_WINERROR_EE(dwError, EE);
 
-    dwError = LsaPstorepBackendGetPasswordInfoW(DnsDomainName, PasswordInfo);
+    if (!DnsDomainName)
+    {
+        // Returns NULL if no default is found.
+        dwError = LsaPstoreGetDefaultDomainW(&defaultDnsDomainName);
+        GOTO_CLEANUP_ON_WINERROR_EE(dwError, EE);
+
+        actualDnsDomainName = defaultDnsDomainName;
+    }
+
+    if (!actualDnsDomainName)
+    {
+        dwError = NERR_SetupNotJoined;
+        GOTO_CLEANUP_EE(EE);
+    }
+
+    dwError = LsaPstorepBackendGetPasswordInfoW(
+                    backendState,
+                    actualDnsDomainName,
+                    &passwordInfo);
     GOTO_CLEANUP_ON_WINERROR_EE(dwError, EE);
 
 cleanup:
-    LSA_PSTORE_LOG_LEAVE_ERROR_EE(dwError, EE);
-    return dwError;
-}
+    if (dwError)
+    {
+        LSA_PSTORE_FREE_PASSWORD_INFO_W(&passwordInfo);
+    }
 
-DWORD
-LsaPstoreSetPasswordInfoA(
-    IN PLSA_MACHINE_PASSWORD_INFO_A PasswordInfo
-    )
-{
-    DWORD dwError = 0;
-    int EE = 0;
+    LW_RTL_FREE(&defaultDnsDomainName);
 
-    dwError = LsaPstorepEnsureInitialized();
-    GOTO_CLEANUP_ON_WINERROR_EE(dwError, EE);
+    *PasswordInfo = passwordInfo;
 
-    dwError = LsaPstorepBackendSetPasswordInfoA(PasswordInfo);
-    GOTO_CLEANUP_ON_WINERROR_EE(dwError, EE);
-
-cleanup:
     LSA_PSTORE_LOG_LEAVE_ERROR_EE(dwError, EE);
     return dwError;
 }
@@ -111,33 +110,48 @@ LsaPstoreSetPasswordInfoW(
 {
     DWORD dwError = 0;
     int EE = 0;
+    PLSA_PSTORE_BACKEND_STATE backendState = NULL;
+    PWSTR defaultDnsDomainName = NULL;
+    BOOLEAN isDefaultDomain = FALSE;
 
-    dwError = LsaPstorepEnsureInitialized();
+    dwError = LsaPstorepCheckPasswordInfoW(PasswordInfo);
     GOTO_CLEANUP_ON_WINERROR_EE(dwError, EE);
 
-    dwError = LsaPstorepBackendSetPasswordInfoW(PasswordInfo);
+    dwError = LsaPstorepEnsureInitialized(&backendState);
     GOTO_CLEANUP_ON_WINERROR_EE(dwError, EE);
+
+    // Returns NULL if no default is set.
+    dwError = LsaPstoreGetDefaultDomainW(&defaultDnsDomainName);
+    GOTO_CLEANUP_ON_WINERROR_EE(dwError, EE);
+
+    dwError = LsaPstorepBackendSetPasswordInfoW(backendState, PasswordInfo);
+    GOTO_CLEANUP_ON_WINERROR_EE(dwError, EE);
+
+    if (!defaultDnsDomainName)
+    {
+        // TODO-Perhaps ignore error here?
+        dwError = LsaPstoreSetDefaultDomainW(PasswordInfo->Account.DnsDomainName);
+        GOTO_CLEANUP_ON_WINERROR_EE(dwError, EE);
+
+        isDefaultDomain = TRUE;
+    }
+    else
+    {
+        isDefaultDomain = LwRtlWC16StringIsEqual(
+                                defaultDnsDomainName,
+                                PasswordInfo->Account.DnsDomainName,
+                                TRUE);
+    }
+
+    if (isDefaultDomain)
+    {
+        dwError = LsaPstorepCallPluginSetPasswordInfo(PasswordInfo);
+        GOTO_CLEANUP_ON_WINERROR_EE(dwError, EE);
+    }
 
 cleanup:
-    LSA_PSTORE_LOG_LEAVE_ERROR_EE(dwError, EE);
-    return dwError;
-}
+    LSA_PSTORE_FREE(&defaultDnsDomainName);
 
-DWORD
-LsaPstoreDeletePasswordInfoA(
-    IN OPTIONAL PCSTR DnsDomainName
-    )
-{
-    DWORD dwError = 0;
-    int EE = 0;
-
-    dwError = LsaPstorepEnsureInitialized();
-    GOTO_CLEANUP_ON_WINERROR_EE(dwError, EE);
-
-    dwError = LsaPstorepBackendDeletePasswordInfoA(DnsDomainName);
-    GOTO_CLEANUP_ON_WINERROR_EE(dwError, EE);
-
-cleanup:
     LSA_PSTORE_LOG_LEAVE_ERROR_EE(dwError, EE);
     return dwError;
 }
@@ -149,33 +163,76 @@ LsaPstoreDeletePasswordInfoW(
 {
     DWORD dwError = 0;
     int EE = 0;
+    PLSA_PSTORE_BACKEND_STATE backendState = NULL;
+    PWSTR defaultDnsDomainName = NULL;
+    BOOLEAN isDefaultDomain = FALSE;
+    PLSA_MACHINE_PASSWORD_INFO_W passwordInfo = NULL;
+    PCWSTR actualDnsDomainName = NULL;
 
-    dwError = LsaPstorepEnsureInitialized();
+    if (DnsDomainName && !LsaPstorepWC16StringIsUpcase(DnsDomainName))
+    {
+        dwError = ERROR_INVALID_PARAMETER;
+        GOTO_CLEANUP_EE(EE);
+    }
+
+    dwError = LsaPstorepEnsureInitialized(&backendState);
     GOTO_CLEANUP_ON_WINERROR_EE(dwError, EE);
 
-    dwError = LsaPstorepBackendDeletePasswordInfoW(DnsDomainName);
+    // Returns NULL if no default is found.
+    dwError = LsaPstoreGetDefaultDomainW(&defaultDnsDomainName);
     GOTO_CLEANUP_ON_WINERROR_EE(dwError, EE);
+
+    actualDnsDomainName = DnsDomainName ? DnsDomainName : defaultDnsDomainName;
+
+    if (defaultDnsDomainName)
+    {
+        if (DnsDomainName)
+        {
+            isDefaultDomain = LwRtlWC16StringIsEqual(
+                                    defaultDnsDomainName,
+                                    DnsDomainName,
+                                    TRUE);
+        }
+        else
+        {
+            isDefaultDomain = TRUE;
+        }
+    }
+
+    if (isDefaultDomain)
+    {
+        // Get information for plugin, but ignore error since best effort.
+        dwError = LsaPstorepBackendGetPasswordInfoW(
+                        backendState,
+                        defaultDnsDomainName,
+                        &passwordInfo);
+        dwError = 0;
+    }
+
+    if (actualDnsDomainName)
+    {
+        dwError = LsaPstorepBackendDeletePasswordInfoW(
+                        backendState,
+                        actualDnsDomainName);
+        GOTO_CLEANUP_ON_WINERROR_EE(dwError, EE);
+    }
+
+    if (isDefaultDomain)
+    {
+        // Ignore error as LSASS can recover
+        dwError = LsaPstoreSetDefaultDomainW(NULL);
+        dwError = 0;
+
+        // TODO-Use defaultDnsDomainName if no passwordInfo available.
+        dwError = LsaPstorepCallPluginDeletePasswordInfo(
+                        passwordInfo ? &passwordInfo->Account : NULL);
+        GOTO_CLEANUP_ON_WINERROR_EE(dwError, EE);
+    }
 
 cleanup:
-    LSA_PSTORE_LOG_LEAVE_ERROR_EE(dwError, EE);
-    return dwError;
-}
+    LW_RTL_FREE(&defaultDnsDomainName);
+    LSA_PSTORE_FREE_PASSWORD_INFO_W(&passwordInfo);
 
-DWORD
-LsaPstoreGetDefaultDomainA(
-    OUT PSTR* DnsDomainName
-    )
-{
-    DWORD dwError = 0;
-    int EE = 0;
-
-    dwError = LsaPstorepEnsureInitialized();
-    GOTO_CLEANUP_ON_WINERROR_EE(dwError, EE);
-
-    dwError = LsaPstorepBackendGetDefaultDomainA(DnsDomainName);
-    GOTO_CLEANUP_ON_WINERROR_EE(dwError, EE);
-
-cleanup:
     LSA_PSTORE_LOG_LEAVE_ERROR_EE(dwError, EE);
     return dwError;
 }
@@ -187,33 +244,25 @@ LsaPstoreGetDefaultDomainW(
 {
     DWORD dwError = 0;
     int EE = 0;
+    PLSA_PSTORE_BACKEND_STATE backendState = NULL;
+    PWSTR dnsDomainName = NULL;
 
-    dwError = LsaPstorepEnsureInitialized();
+    dwError = LsaPstorepEnsureInitialized(&backendState);
     GOTO_CLEANUP_ON_WINERROR_EE(dwError, EE);
 
-    dwError = LsaPstorepBackendGetDefaultDomainW(DnsDomainName);
-    GOTO_CLEANUP_ON_WINERROR_EE(dwError, EE);
-
-cleanup:
-    LSA_PSTORE_LOG_LEAVE_ERROR_EE(dwError, EE);
-    return dwError;
-}
-
-DWORD
-LsaPstoreSetDefaultDomainA(
-    IN OPTIONAL PCSTR DnsDomainName
-    )
-{
-    DWORD dwError = 0;
-    int EE = 0;
-
-    dwError = LsaPstorepEnsureInitialized();
-    GOTO_CLEANUP_ON_WINERROR_EE(dwError, EE);
-
-    dwError = LsaPstorepBackendSetDefaultDomainA(DnsDomainName);
+    dwError = LsaPstorepBackendGetDefaultDomainW(
+                    backendState,
+                    &dnsDomainName);
     GOTO_CLEANUP_ON_WINERROR_EE(dwError, EE);
 
 cleanup:
+    if (dwError)
+    {
+        LSA_PSTORE_FREE(&dnsDomainName);
+    }
+
+    *DnsDomainName = dnsDomainName;
+
     LSA_PSTORE_LOG_LEAVE_ERROR_EE(dwError, EE);
     return dwError;
 }
@@ -225,31 +274,18 @@ LsaPstoreSetDefaultDomainW(
 {
     DWORD dwError = 0;
     int EE = 0;
+    PLSA_PSTORE_BACKEND_STATE backendState = NULL;
 
-    dwError = LsaPstorepEnsureInitialized();
+    if (DnsDomainName && !LsaPstorepWC16StringIsUpcase(DnsDomainName))
+    {
+        dwError = ERROR_INVALID_PARAMETER;
+        GOTO_CLEANUP_EE(EE);
+    }
+
+    dwError = LsaPstorepEnsureInitialized(&backendState);
     GOTO_CLEANUP_ON_WINERROR_EE(dwError, EE);
 
-    dwError = LsaPstorepBackendSetDefaultDomainW(DnsDomainName);
-    GOTO_CLEANUP_ON_WINERROR_EE(dwError, EE);
-
-cleanup:
-    LSA_PSTORE_LOG_LEAVE_ERROR_EE(dwError, EE);
-    return dwError;
-}
-
-DWORD
-LsaPstoreGetJoinedDomainsA(
-    OUT PSTR** DnsDomainNames,
-    OUT PDWORD Count
-    )
-{
-    DWORD dwError = 0;
-    int EE = 0;
-
-    dwError = LsaPstorepEnsureInitialized();
-    GOTO_CLEANUP_ON_WINERROR_EE(dwError, EE);
-
-    dwError = LsaPstorepBackendGetJoinedDomainsA(DnsDomainNames, Count);
+    dwError = LsaPstorepBackendSetDefaultDomainW(backendState, DnsDomainName);
     GOTO_CLEANUP_ON_WINERROR_EE(dwError, EE);
 
 cleanup:
@@ -265,12 +301,18 @@ LsaPstoreGetJoinedDomainsW(
 {
     DWORD dwError = 0;
     int EE = 0;
+    PLSA_PSTORE_BACKEND_STATE backendState = NULL;
 
-    dwError = LsaPstorepEnsureInitialized();
+    dwError = LsaPstorepEnsureInitialized(&backendState);
     GOTO_CLEANUP_ON_WINERROR_EE(dwError, EE);
 
-    dwError = LsaPstorepBackendGetJoinedDomainsW(DnsDomainNames, Count);
+    dwError = LsaPstorepBackendGetJoinedDomainsW(
+                    backendState,
+                    DnsDomainNames,
+                    Count);
     GOTO_CLEANUP_ON_WINERROR_EE(dwError, EE);
+
+    // VALIDATE...
 
 cleanup:
     LSA_PSTORE_LOG_LEAVE_ERROR_EE(dwError, EE);

lsass/include/lsa/lsapstore-api.h
@@ -183,14 +183,11 @@ LsaPstoreGetDefaultDomainW(
 ///<
 /// Get default joined domain name.
 ///
-/// TODO-Resolve inconsistency wrt returning NULL vs NERR_SetupNotJoined
-///
 /// @param[out] DnsDomainName - Returns the default domain join DNS domain
-///     name, or NULL if none.  Free with LwFreeMemory().
+///     name, or NULL if none.  Free with LsaPstoreFreeMemory().
 ///
 /// @return Windows error code
 /// @retval ERROR_SUCCESS on success
-/// @retval NERR_SetupNotJoined if no default domain join is set.
 /// @retval !ERROR_SUCCESS on other failure
 ///
 
@@ -231,7 +228,7 @@ LsaPstoreGetJoinedDomainsW(
 ///
 /// @param[out] DnsDomainNames - Returns DNS domain names for which there
 ///     is join information.  Returns NULL if not joined to any domains.
-///     Free with LwFreeStringArray().
+///     Free with LsaPstoreFreeStringArrayW().
 ///
 /// @param[out] Count - Returns count of domains for which there is password
 ///     info.  Returns 0 if not joined to any domains.
@@ -282,8 +279,8 @@ LsaPstoreFreeStringArrayW(
         { \
             LsaPstoreFreeStringArrayA(*(pStringArray), *(pCount)); \
             *(pStringArray) = NULL; \
-            *(pCount) = 0; \
         } \
+        *(pCount) = 0; \
     } while (0)
 
 #define LSA_PSTORE_FREE_STRING_ARRAY_W(pStringArray, pCount) \
@@ -292,8 +289,8 @@ LsaPstoreFreeStringArrayW(
         { \
             LsaPstoreFreeStringArrayW(*(pStringArray), *(pCount)); \
             *(pStringArray) = NULL; \
-            *(pCount) = 0; \
         } \
+        *(pCount) = 0; \
     } while (0)
 
 VOID

lsass/pstore/MakeKitBuild
@@ -6,11 +6,10 @@ make()
         LIB=lsapstore \
         SOURCES="\
            lsapstore-backend-legacy.c \
-           lsapstore-backend-legacy-convert.c \
            lsapstore-backend-legacy-internal.c \
-           lsapstore-backend-wrap-a.c \
            lsapstore-init.c \
            lsapstore-main.c \
+           lsapstore-main-a.c \
            lsapstore-plugin.c \
            lsapstore-utils.c \
            " \

lsass/pstore/Makefile.am
@@ -6,11 +6,10 @@ lib_LTLIBRARIES = liblsapstore.la
 
 liblsapstore_la_SOURCES =  \
         lsapstore-backend-legacy.c \
-        lsapstore-backend-legacy-convert.c \
         lsapstore-backend-legacy-internal.c \
-        lsapstore-backend-wrap-a.c \
         lsapstore-init.c \
         lsapstore-main.c \
+        lsapstore-main-a.c \
         lsapstore-plugin.c \
         lsapstore-utils.c
 

lsass/pstore/lsapstore-backend-legacy-convert.c
@@ -1,430 +1,0 @@
-/* Editor Settings: expandtabs and use 4 spaces for indentation
- * ex: set softtabstop=4 tabstop=8 expandtab shiftwidth=4: *
- * -*- mode: c, c-basic-offset: 4 -*- */
-
-/*
- * Copyright (c) Likewise Software.  All rights Reserved.
- *
- * This library is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation; either version 2.1 of the license, or (at
- * your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser
- * General Public License for more details.  You should have received a copy
- * of the GNU Lesser General Public License along with this program.  If
- * not, see <http://www.gnu.org/licenses/>.
- *
- * LIKEWISE SOFTWARE MAKES THIS SOFTWARE AVAILABLE UNDER OTHER LICENSING
- * TERMS AS WELL.  IF YOU HAVE ENTERED INTO A SEPARATE LICENSE AGREEMENT
- * WITH LIKEWISE SOFTWARE, THEN YOU MAY ELECT TO USE THE SOFTWARE UNDER THE
- * TERMS OF THAT SOFTWARE LICENSE AGREEMENT INSTEAD OF THE TERMS OF THE GNU
- * LESSER GENERAL PUBLIC LICENSE, NOTWITHSTANDING THE ABOVE NOTICE.  IF YOU
- * HAVE QUESTIONS, OR WISH TO REQUEST A COPY OF THE ALTERNATE LICENSING
- * TERMS OFFERED BY LIKEWISE SOFTWARE, PLEASE CONTACT LIKEWISE SOFTWARE AT
- * license@likewise.com
- */
-
-/*
- * Copyright (C) Likewise Software. All rights reserved.
- *  
- * Module Name:
- *
- *     lsapstore-backend-legacy-convert.c
- *
- * Abstract:
- *
- *     LSASS Password Store API Implementation
- *
- *     Legacy Backend Conversion Implementation
- *
- * Authors: Danilo Almeida (dalmeida@likewise.com)
- */
-
-#include "lsapstore-backend-legacy.h"
-#include "lsapstore-includes.h"
-#include <lwstr.h>
-#include <lwmem.h>
-// These includes work around bugs in lwstr.h:
-#include <string.h>
-#include <wc16str.h>
-// Needed for SIZEOF_TIME_T:
-#include "config.h"
-
-static
-inline
-PCWSTR
-LocalStringOrEmptyW(
-    IN OPTIONAL PCWSTR String
-    )
-{
-    static PCWSTR empty = { 0 };
-    return String ? String : empty;
-}
-
-static
-inline
-DWORD
-LwpsConvertWC16StringDuplicateOrNull(
-    OUT PWSTR* ppszNewString,
-    IN PCWSTR pszOriginalString
-)
-{
-    DWORD dwError = 0;
-
-    if (pszOriginalString)
-    {
-        dwError = LwNtStatusToWin32Error(LwRtlWC16StringDuplicate(
-                        ppszNewString,
-                        pszOriginalString));
-    }
-    else
-    {
-        *ppszNewString = NULL;
-    }
-
-    return dwError;
-}
-
-static
-inline
-DWORD
-LwpsConvertWc16StrDupOrNull(
-    IN OPTIONAL PCWSTR InputString,
-    OUT PWSTR* OutputString
-    )
-{
-    DWORD dwError = 0;
-
-    if (InputString)
-    {
-        dwError = LwAllocateWc16String(OutputString, InputString);
-    }
-    else
-    {
-        *OutputString = NULL;
-    }
-
-    return dwError;
-}
-
-static
-inline
-DWORD
-LwpsConvertTimeWindowsToUnix(
-    IN LONG64 WindowsTime,
-    OUT time_t* pUnixTime
-    )
-{
-    DWORD dwError = 0;
-    LONG64 unixTime = 0;
-
-    if (WindowsTime < 0)
-    {
-        dwError = ERROR_INVALID_PARAMETER;
-        GOTO_CLEANUP_ON_WINERROR(dwError);
-    }
-
-    unixTime = WindowsTime/10000000LL - 11644473600LL;
-
-    // Ensure the range is within time_t.
-    if (unixTime != (time_t) unixTime)
-    {
-        dwError = ERROR_ARITHMETIC_OVERFLOW;
-        GOTO_CLEANUP_ON_WINERROR(dwError);
-    }
-
-cleanup:
-    if (dwError)
-    {
-        unixTime = 0;
-    }
-
-    *pUnixTime = (time_t) unixTime;
-
-    return 0;
-}
-
-static
-inline
-DWORD
-LwpsConvertTimeUnixToWindows(
-    IN time_t UnixTime,
-    OUT PLONG64 pWindowsTime
-    )
-{
-    DWORD dwError = 0;
-    LONG64 windowsTime = 0;
-
-#if SIZEOF_TIME_T > 4
-    if ((LONG64) UnixTime < - 11644473600LL)
-    {
-        dwError = ERROR_INVALID_PARAMETER;
-        GOTO_CLEANUP_ON_WINERROR(dwError);
-    }
-#endif
-
-    windowsTime = (LONG64) UnixTime + 11644473600LL;
-    if (windowsTime < 0)
-    {
-        dwError = ERROR_ARITHMETIC_OVERFLOW;
-        GOTO_CLEANUP_ON_WINERROR(dwError);
-    }
-    windowsTime *= 10000000LL;
-    if (windowsTime < 0)
-    {
-        dwError = ERROR_ARITHMETIC_OVERFLOW;
-        GOTO_CLEANUP_ON_WINERROR(dwError);
-    }
-
-cleanup:
-    if (dwError)
-    {
-        windowsTime = 0;
-    }
-
-    *pWindowsTime = windowsTime;
-
-    return 0;
-}
-
-static
-LSA_MACHINE_ACCOUNT_TYPE
-LwpsConvertSchannelToAccountType(
-    DWORD SchannelType
-    )
-{
-    LSA_MACHINE_ACCOUNT_TYPE accountType = 0;
-
-    switch (SchannelType)
-    {
-        case 2: // SCHANNEL_WKSTA
-            accountType = LSA_MACHINE_ACCOUNT_TYPE_WORKSTATION;
-            break;
-        case 4: // SCHANNEL_DOMAIN
-            accountType = LSA_MACHINE_ACCOUNT_TYPE_DC;
-            break;
-        case 6: // SCHANNEL_BDC
-            accountType = LSA_MACHINE_ACCOUNT_TYPE_BDC;
-            break;
-        default:
-            // Default to workstation
-            accountType = LSA_MACHINE_ACCOUNT_TYPE_WORKSTATION;
-            break;
-    }
-
-    return accountType;
-}
-
-static
-DWORD
-LwpsConvertAccountTypeToSchannel(
-    LSA_MACHINE_ACCOUNT_TYPE Accountype
-    )
-{
-    DWORD schannelType = 0;
-
-    switch (Accountype)
-    {
-        case LSA_MACHINE_ACCOUNT_TYPE_WORKSTATION:
-            schannelType = 2; // SCHANNEL_WKSTA
-            break;
-        case LSA_MACHINE_ACCOUNT_TYPE_DC:
-            schannelType = 4; // SCHANNEL_DOMAIN
-            break;
-        case LSA_MACHINE_ACCOUNT_TYPE_BDC:
-            schannelType = 6; // SCHANNEL_BDC
-            break;
-        default:
-            // Default to workstation
-            schannelType = 2; // SCHANNEL_WKSTA
-            break;
-    }
-
-    return schannelType;
-}
-
-DWORD
-LwpsConvertFillMachinePasswordInfoW(
-    IN PLWPS_LEGACY_PASSWORD_INFO pLegacyPasswordInfo,
-    OUT PLSA_MACHINE_PASSWORD_INFO_W pPasswordInfo
-    )
-{
-    DWORD dwError = 0;
-    int EE = 0;
-    PLSA_MACHINE_ACCOUNT_INFO_W pAccountInfo = &pPasswordInfo->Account;
-
-    //
-    // Account Portion
-    //
-
-    dwError = LwpsConvertWC16StringDuplicateOrNull(
-                    &pAccountInfo->DnsDomainName,
-                    pLegacyPasswordInfo->pwszDnsDomainName);
-    GOTO_CLEANUP_ON_WINERROR_EE(dwError, EE);
-
-    LsaPstorepWC16StringUpcase(pAccountInfo->DnsDomainName);
-
-    dwError = LwpsConvertWC16StringDuplicateOrNull(
-                    &pAccountInfo->NetbiosDomainName,
-                    pLegacyPasswordInfo->pwszDomainName);
-    GOTO_CLEANUP_ON_WINERROR_EE(dwError, EE);
-
-    LsaPstorepWC16StringUpcase(pAccountInfo->NetbiosDomainName);
-
-    dwError = LwpsConvertWC16StringDuplicateOrNull(
-                    &pAccountInfo->DomainSid,
-                    pLegacyPasswordInfo->pwszSID);
-    GOTO_CLEANUP_ON_WINERROR_EE(dwError, EE);
-
-    dwError = LwpsConvertWC16StringDuplicateOrNull(
-                    &pAccountInfo->SamAccountName,
-                    pLegacyPasswordInfo->pwszMachineAccount);
-    GOTO_CLEANUP_ON_WINERROR_EE(dwError, EE);
-
-    LsaPstorepWC16StringUpcase(pAccountInfo->SamAccountName);
-
-    pAccountInfo->Type = LwpsConvertSchannelToAccountType(pLegacyPasswordInfo->dwSchannelType);
-
-    // TODO-2010/12/03-dalmeida - Hard-coded for now.
-    pAccountInfo->KeyVersionNumber = 0;
-
-    dwError = LwNtStatusToWin32Error(LwRtlWC16StringAllocatePrintfW(
-                    &pAccountInfo->Fqdn,
-                    L"%ws.%ws",
-                    LocalStringOrEmptyW(pLegacyPasswordInfo->pwszHostname),
-                    LocalStringOrEmptyW(pLegacyPasswordInfo->pwszHostDnsDomain)));
-    GOTO_CLEANUP_ON_WINERROR_EE(dwError, EE);
-
-    LsaPstorepWC16StringDowncase(pAccountInfo->Fqdn);
-
-    dwError = LwpsConvertTimeUnixToWindows(
-                    pLegacyPasswordInfo->last_change_time,
-                    &pAccountInfo->LastChangeTime);
-    GOTO_CLEANUP_ON_WINERROR_EE(dwError, EE);
-
-    //
-    // Password portion
-    //
-
-    dwError = LwpsConvertWC16StringDuplicateOrNull(
-                    &pPasswordInfo->Password,
-                    pLegacyPasswordInfo->pwszMachinePassword);
-    GOTO_CLEANUP_ON_WINERROR_EE(dwError, EE);
-
-cleanup:
-    if (dwError)
-    {
-        LsaPstorepFreePasswordInfoContentsW(pPasswordInfo);
-    }
-
-    LSA_PSTORE_LOG_LEAVE_ERROR_EE(dwError, EE);
-    return dwError;    
-}
-
-DWORD
-LwpsConvertAllocateFromMachinePasswordInfoW(
-    IN PLSA_MACHINE_PASSWORD_INFO_W pPasswordInfo,
-    OUT PLWPS_LEGACY_PASSWORD_INFO* ppLegacyPasswordInfo
-    )
-{
-    DWORD dwError = 0;
-    int EE = 0;
-    PLWPS_LEGACY_PASSWORD_INFO pLegacyPasswordInfo = NULL;
-    PLSA_MACHINE_ACCOUNT_INFO_W pAccountInfo = &pPasswordInfo->Account;
-
-    dwError = LwAllocateMemory(sizeof(*pLegacyPasswordInfo), OUT_PPVOID(&pLegacyPasswordInfo));
-    GOTO_CLEANUP_ON_WINERROR_EE(dwError, EE);
-
-    dwError = LwpsConvertWc16StrDupOrNull(
-                    pAccountInfo->DnsDomainName,
-                    &pLegacyPasswordInfo->pwszDnsDomainName);
-    GOTO_CLEANUP_ON_WINERROR_EE(dwError, EE);
-
-    LwWc16sToUpper(pLegacyPasswordInfo->pwszDnsDomainName);
-
-    dwError = LwpsConvertWc16StrDupOrNull(
-                    pAccountInfo->NetbiosDomainName,
-                    &pLegacyPasswordInfo->pwszDomainName);
-    GOTO_CLEANUP_ON_WINERROR_EE(dwError, EE);
-
-    LwWc16sToUpper(pLegacyPasswordInfo->pwszDomainName);
-
-    dwError = LwpsConvertWc16StrDupOrNull(
-                    pAccountInfo->DomainSid,
-                    &pLegacyPasswordInfo->pwszSID);
-    GOTO_CLEANUP_ON_WINERROR_EE(dwError, EE);
-
-    dwError = LwpsConvertWc16StrDupOrNull(
-                    pAccountInfo->SamAccountName,
-                    &pLegacyPasswordInfo->pwszMachineAccount);
-    GOTO_CLEANUP_ON_WINERROR_EE(dwError, EE);
-
-    LwWc16sToUpper(pLegacyPasswordInfo->pwszMachineAccount);
-
-    pLegacyPasswordInfo->dwSchannelType = LwpsConvertAccountTypeToSchannel(pAccountInfo->Type);
-
-    dwError = LwpsConvertWc16StrDupOrNull(
-                    pAccountInfo->Fqdn,
-                    &pLegacyPasswordInfo->pwszHostname);
-    GOTO_CLEANUP_ON_WINERROR_EE(dwError, EE);
-
-    LwWc16sToLower(pLegacyPasswordInfo->pwszHostname);
-
-    // Split FQDN
-    if (pLegacyPasswordInfo->pwszHostname)
-    {
-        PWSTR cursor = pLegacyPasswordInfo->pwszHostname;
-        while (*cursor && (*cursor != '.'))
-        {
-            cursor++;
-        }
-        if (*cursor)
-        {
-            *cursor = 0;
-            cursor++;
-
-            dwError = LwpsConvertWc16StrDupOrNull(
-                            cursor,
-                            &pLegacyPasswordInfo->pwszHostDnsDomain);
-            GOTO_CLEANUP_ON_WINERROR_EE(dwError, EE);
-
-            LwWc16sToLower(pLegacyPasswordInfo->pwszHostDnsDomain);
-        }
-    }
-
-    if (pAccountInfo->LastChangeTime)
-    {
-        dwError = LwpsConvertTimeWindowsToUnix(
-                        pAccountInfo->LastChangeTime,
-                        &pLegacyPasswordInfo->last_change_time);
-        GOTO_CLEANUP_ON_WINERROR_EE(dwError, EE);
-    }
-    else
-    {
-        // Treat 0 as "now".
-        pLegacyPasswordInfo->last_change_time = time(NULL);
-    }
-
-    dwError = LwpsConvertWc16StrDupOrNull(
-                    pPasswordInfo->Password,
-                    &pLegacyPasswordInfo->pwszMachinePassword);
-    GOTO_CLEANUP_ON_WINERROR_EE(dwError, EE);
-
-cleanup:
-    if (dwError)
-    {
-        if (pLegacyPasswordInfo)
-        {
-            LwpsLegacyFreePassword(pLegacyPasswordInfo);
-            pLegacyPasswordInfo = NULL;
-        }
-    }
-
-    *ppLegacyPasswordInfo = pLegacyPasswordInfo;
-
-    LSA_PSTORE_LOG_LEAVE_ERROR_EE(dwError, EE);
-    return dwError;    
-}

commit 29c4971fda35dbfc2ba17cdc7bc6ff298deb4f51

Fix domain leave, broken by r54873.
CR:  dalmeida


git-svn-id: svn+ssh://source-control.beyondtrust.com/sources/repo/bt-dev/trunk/Platform/src/linux@54896 9fc1c11e-11b2-4e14-94d8-6c5e05775969

lsass/pstore/lsapstore-main.c
@@ -59,12 +59,6 @@ LsaPstoreGetPasswordInfoW(
     PCWSTR actualDnsDomainName = DnsDomainName;
     PLSA_MACHINE_PASSWORD_INFO_W passwordInfo = NULL;
 
-    if (DnsDomainName && !LsaPstorepWC16StringIsUpcase(DnsDomainName))
-    {
-        dwError = ERROR_INVALID_PARAMETER;
-        GOTO_CLEANUP_EE(EE);
-    }
-
     dwError = LsaPstorepEnsureInitialized(&backendState);
     GOTO_CLEANUP_ON_WINERROR_EE(dwError, EE);
 
@@ -168,12 +162,6 @@ LsaPstoreDeletePasswordInfoW(
     BOOLEAN isDefaultDomain = FALSE;
     PLSA_MACHINE_PASSWORD_INFO_W passwordInfo = NULL;
     PCWSTR actualDnsDomainName = NULL;
-
-    if (DnsDomainName && !LsaPstorepWC16StringIsUpcase(DnsDomainName))
-    {
-        dwError = ERROR_INVALID_PARAMETER;
-        GOTO_CLEANUP_EE(EE);
-    }
 
     dwError = LsaPstorepEnsureInitialized(&backendState);
     GOTO_CLEANUP_ON_WINERROR_EE(dwError, EE);