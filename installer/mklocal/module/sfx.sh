6e25e613a25c8fd1f6a67cdd1ab7a1ae0c7933f2
bkoropoff <bkoropoff@9fc1c11e-11b2-4e14-94d8-6c5e05775969>
2011-03-24 17:24:12 +0000

    installer: clean up sfx building
    git-svn-id: svn+ssh://source-control.beyondtrust.com/sources/repo/bt-dev/trunk/Platform/src/linux@57585 9fc1c11e-11b2-4e14-94d8-6c5e05775969

--- installer/mklocal/module/sfx.sh
+++ installer/mklocal/module/sfx.sh
@@ -4,10 +4,11 @@ DEPENDS="package"
 
 lw_sfx()
 {
-    mk_push_vars FILES SCRIPT SFX PKGDIRS FORMAT BASE UPGRADE OBSOLETE GUI
+    mk_push_vars FILES PACKAGES SFX PKGDIRS FORMAT BASE UPGRADE OBSOLETE GUI
     mk_parse_params
 
-    for _pkgname in "$@"
+    mk_unquote_list "$PACKAGES"
+    for _pkgname
     do
         mk_quote "@${MK_PACKAGE_DIR}/${FORMAT}/${_pkgname}"
         PKGDIRS="$PKGDIRS $result"
@@ -15,8 +16,8 @@ lw_sfx()
 
     mk_target \
         TARGET="@sfx/$SFX.$MK_HOST_OS.$MK_HOST_ARCH.$FORMAT.sh" \
-        DEPS="$PKGDIRS" \
-        _lw_sfx %FILES %SCRIPT %FORMAT %BASE %UPGRADE %OBSOLETE %GUI \
+        DEPS="$PKGDIRS $FILES" \
+        _lw_sfx %FILES %FORMAT %BASE %UPGRADE %OBSOLETE %GUI \
         '$@' "*$PKGDIRS"
 
     _sfx_target="$result"
@@ -53,6 +54,8 @@ _lw_sfx()
     SFX="$1"
     shift
 
+    mk_msg "${SFX#sfx/}"
+
     LABEL="${SFX##*/}"
     LABEL="${LABEL%.*}"
 
@@ -62,23 +65,31 @@ _lw_sfx()
     mk_resolve_resource "makeself/makeself-header.sh"
     MAKESELF_HEADER="$result"
 
+    mk_resolve_resource "makeself/install.sh"
+    INSTALL_SH="$result"
+
     mk_resolve_file ".sfx-temp-$$"
     TEMP_DIR="$result"
 
-    mk_safe_rm "$TEMP_DIR"
-    mk_mkdir "$TEMP_DIR/${LABEL}"
+    mk_quote "$TEMP_DIR"
+    trap "mk_safe_rm $result" 0
 
-    for _file in ${FILES} ${SCRIPT}
+    mk_safe_rm "$TEMP_DIR"
+    mk_mkdir "$TEMP_DIR/$LABEL"
+
+    mk_run_or_fail cp "$INSTALL_SH" "$TEMP_DIR/$LABEL/"
+
+    for _file in ${FILES}
     do
         mk_resolve_file "$_file"
-        mk_run_or_fail cp "$result" "$TEMP_DIR/${LABEL}/"
+        mk_run_or_fail cp "$result" "$TEMP_DIR/$LABEL/"
     done
 
-    mk_mkdir "$TEMP_DIR/${LABEL}/packages"
+    mk_mkdir "$TEMP_DIR/$LABEL/packages"
 
-    for _pkgdir in "$@"
+    for _pkgdir
     do
-        mk_run_or_fail cp "$_pkgdir"/* "$TEMP_DIR/${LABEL}/packages/"
+        mk_run_or_fail cp "$_pkgdir"/* "$TEMP_DIR/$LABEL/packages/"
     done
 
     {
@@ -87,22 +98,20 @@ _lw_sfx()
         echo "INSTALL_OBSOLETE_PACKAGES=\"$OBSOLETE\""
         echo "INSTALL_BASE_PACKAGE=\"$BASE\""
         echo "INSTALL_GUI_PACKAGE=\"$GUI\""
-    } > "$TEMP_DIR/${LABEL}/MANIFEST" || mk_fail "could not write manifest"
+    } > "$TEMP_DIR/$LABEL/MANIFEST" || mk_fail "could not write manifest"
     
     mk_mkdirname "$SFX"
 
     TMP_DIR="$TEMP_DIR"
     export TMP_DIR
 
-    mk_msg "${SFX#sfx/}"
-
     mk_run_quiet_or_fail \
         "$MAKESELF_SH" \
         --nox11 --notemp \
         --header "$MAKESELF_HEADER" \
-        "$TEMP_DIR/${LABEL}" \
+        "$TEMP_DIR/$LABEL" \
         "$SFX" \
         "$LABEL" \
-        "./`basename $SCRIPT`" \
+        "./install.sh" \
         --echo-dir "$LABEL"
 }
