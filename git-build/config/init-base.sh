82fd79a997115d327c5423ea747eab1ace285c06
Scott Salley <ssalley@beyondtrust.com>
2011-05-17 17:49:08 +0000

    git-build:config: Fix up paths and remove unused init scripts
    (git-build: r59372)

--- git-build/config/init-base.sh
+++ git-build/config/init-base.sh
@@ -76,7 +76,7 @@ then
         unalias _test_alias
         unalias aliastest
         # Resource this script
-        . /opt/likewise/bin/init-base.sh
+        . EXECDIR/init-base.sh
         exit $?
     fi
 
