1b0292cac8162dcef1ca13a74c72084a9687c826
Unknown <unknown@beyondtrust.com>
2011-06-15 17:57:57 +0000

    MERGE: -c 60262 /trunk/Platform -> /branches/Platform-6.1
    lsass:ad-provider: Correctly convert STATUS_PASSWORD_MUST_CHANGE when returned from NetrSamLogonEx
    so it does not cause schannel connection reset.
    
    (lsass: r60282)

--- lsass/server/auth-providers/ad-open-provider/adnetapi.c
+++ lsass/server/auth-providers/ad-open-provider/adnetapi.c
@@ -1896,6 +1896,9 @@ AD_NetlogonAuthenticationUserEx(
     case STATUS_PASSWORD_EXPIRED:
         dwError = LW_ERROR_PASSWORD_EXPIRED;
         break;
+    case STATUS_PASSWORD_MUST_CHANGE:
+        dwError = ERROR_PASSWORD_MUST_CHANGE;
+        break;
     case STATUS_WRONG_PASSWORD:
         dwError = LW_ERROR_INVALID_PASSWORD;
         break;
