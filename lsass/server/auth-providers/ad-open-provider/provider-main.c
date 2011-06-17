7112b12e680f764d7075f4f5cf4bb59c65987c9d
CORP\abernstein <CORP\abernstein@9fc1c11e-11b2-4e14-94d8-6c5e05775969>
2011-06-17 00:47:48 +0000

    BUG 12919: Port bug fix for 5.4 to platform. r60351 | CORP\abernstein | 2011-06-16 16:44:58 -0700 (Thu, 16 Jun 2011) | 3 lines
    BUG 12919: Move LsaUmCleanup/LsaDmCleanup before the provider data
    cleanup occurs, as these routines use data in this context.
    
    
    
    git-svn-id: svn+ssh://source-control.beyondtrust.com/sources/repo/bt-dev/trunk/Platform/src/linux@60370 9fc1c11e-11b2-4e14-94d8-6c5e05775969

--- lsass/server/auth-providers/ad-open-provider/provider-main.c
+++ lsass/server/auth-providers/ad-open-provider/provider-main.c
@@ -1291,12 +1291,6 @@ AD_Deactivate(
 
     AD_MachineCredentialsCacheClear(pState);
 
-    if (pState->pProviderData)
-    {
-        ADProviderFreeProviderData(pState->pProviderData);
-        pState->pProviderData = NULL;
-    }
-
     if (pState->MediaSenseHandle)
     {
         MediaSenseStop(&pState->MediaSenseHandle);
@@ -1314,6 +1308,12 @@ AD_Deactivate(
     pState->pPcache = NULL;
 
     ADCacheSafeClose(&pState->hCacheConnection);
+
+    if (pState->pProviderData)
+    {
+        ADProviderFreeProviderData(pState->pProviderData);
+        pState->pProviderData = NULL;
+    }
 
     return dwError;
 }
