BeyondTrust/pbis-open

commit 8d68fe68c328c70c91f0f08e34cd72d6665b1669

Fix for bug #11743.  Add lock around setting an alternate domain for NTLM with multi-tenancy.
CR:  dalmeida


git-svn-id: svn+ssh://source-control.beyondtrust.com/sources/repo/bt-dev/trunk/Platform/src/linux@60191 9fc1c11e-11b2-4e14-94d8-6c5e05775969

lsass/server/ntlm/structs.h
@@ -257,6 +257,7 @@ typedef struct _NTLM_CREDENTIALS
     DWORD dwCredDirection;
     PSTR pszDomainName;
     LONG nRefCount;
+    pthread_mutex_t Mutex;
 } NTLM_CREDENTIALS, *PNTLM_CREDENTIALS;
 
 typedef struct _NTLM_CONFIG

lsass/server/ntlm/acceptsecctxt.c
@@ -223,19 +223,37 @@ NtlmCreateChallengeContext(
     PSTR pDomainName = NULL;
     PSTR pDnsServerName = NULL;
     PSTR pDnsDomainName = NULL;
+    BOOLEAN bInLock = FALSE;
 
     *ppNtlmContext = NULL;
 
     dwError = NtlmCreateContext(hCred, &pNtlmContext);
     BAIL_ON_LSA_ERROR(dwError);
 
-    dwError = NtlmGetNameInformation(
-        pCred ? pCred->pszDomainName : NULL,
-        &pServerName,
-        &pDomainName,
-        &pDnsServerName,
-        &pDnsDomainName);
-    BAIL_ON_LSA_ERROR(dwError);
+    if (pCred)
+    {
+        NTLM_LOCK_MUTEX(bInLock, &pCred->Mutex);
+
+        dwError = NtlmGetNameInformation(
+            pCred->pszDomainName,
+            &pServerName,
+            &pDomainName,
+            &pDnsServerName,
+            &pDnsDomainName);
+        BAIL_ON_LSA_ERROR(dwError);
+
+        NTLM_UNLOCK_MUTEX(bInLock, &pCred->Mutex);
+    }
+    else
+    {
+        dwError = NtlmGetNameInformation(
+            NULL,
+            &pServerName,
+            &pDomainName,
+            &pDnsServerName,
+            &pDnsDomainName);
+        BAIL_ON_LSA_ERROR(dwError);
+    }
 
     dwError = NtlmGetRandomBuffer(
         pNtlmContext->Challenge,
@@ -265,10 +283,17 @@ NtlmCreateChallengeContext(
 
 cleanup:
     *ppNtlmContext = pNtlmContext;
+
     LW_SAFE_FREE_STRING(pServerName);
     LW_SAFE_FREE_STRING(pDomainName);
     LW_SAFE_FREE_STRING(pDnsServerName);
     LW_SAFE_FREE_STRING(pDnsDomainName);
+
+    if (pCred)
+    {
+        NTLM_UNLOCK_MUTEX(bInLock, &pCred->Mutex);
+    }
+
     return dwError;
 
 error:
@@ -664,6 +689,7 @@ NtlmValidateResponse(
     PSTR pDomainInstance = NULL;
     BYTE sessionNonce[MD5_DIGEST_LENGTH];
     BYTE sessionHashUntrunc[MD5_DIGEST_LENGTH];
+    BOOLEAN bInLock = FALSE;
 
     memset(&Params, 0, sizeof(Params));
     memset(&Challenge, 0, sizeof(Challenge));
@@ -788,13 +814,20 @@ NtlmValidateResponse(
 
     Params.pszWorkstation = pWorkstation;
 
-    if (pCred && pCred->pszDomainName)
+    if (pCred)
     {
-        dwError = LwAllocateStringPrintf(
-                      &pDomainInstance,
-                      ":%s",
-                      pCred->pszDomainName);
-        BAIL_ON_LSA_ERROR(dwError);
+        NTLM_LOCK_MUTEX(bInLock, &pCred->Mutex);
+
+        if (pCred->pszDomainName)
+        {
+            dwError = LwAllocateStringPrintf(
+                          &pDomainInstance,
+                          ":%s",
+                          pCred->pszDomainName);
+            BAIL_ON_LSA_ERROR(dwError);
+        }
+
+        NTLM_UNLOCK_MUTEX(bInLock, &pCred->Mutex);
     }
 
     dwError = LsaSrvAuthenticateUserEx(
@@ -828,6 +861,11 @@ NtlmValidateResponse(
     pUserInfo = NULL;
 
 cleanup:
+
+    if (pCred)
+    {
+        NTLM_UNLOCK_MUTEX(bInLock, &pCred->Mutex);
+    }
 
     if (pUserInfo)
     {

lsass/server/ntlm/initsecctxt.c
@@ -70,6 +70,7 @@ NtlmServerInitializeSecurityContext(
     PSTR pDomain = NULL;
     PNTLM_CHALLENGE_MESSAGE pMessage = NULL;
     DWORD dwMessageSize = 0;
+    BOOLEAN bInLock = FALSE;
 
     pOutput->pvBuffer = NULL;
 
@@ -80,14 +81,30 @@ NtlmServerInitializeSecurityContext(
 
     if (!pNtlmContext)
     {
-        dwError = NtlmGetNameInformation(
-                pCred ? pCred->pszDomainName : NULL,
-                &pWorkstation,
-                &pDomain,
-                NULL,
-                NULL);
-        BAIL_ON_LSA_ERROR(dwError);
+        if (pCred)
+        {
+            NTLM_LOCK_MUTEX(bInLock, &pCred->Mutex);
 
+            dwError = NtlmGetNameInformation(
+                    pCred->pszDomainName,
+                    &pWorkstation,
+                    &pDomain,
+                    NULL,
+                    NULL);
+            BAIL_ON_LSA_ERROR(dwError);
+
+            NTLM_UNLOCK_MUTEX(bInLock, &pCred->Mutex);
+        }
+        else
+        {
+            dwError = NtlmGetNameInformation(
+                    NULL,
+                    &pWorkstation,
+                    &pDomain,
+                    NULL,
+                    NULL);
+            BAIL_ON_LSA_ERROR(dwError);
+        }
 
         // If we start with a NULL context, create a negotiate message
         dwError = NtlmCreateNegotiateContext(
@@ -138,13 +155,21 @@ NtlmServerInitializeSecurityContext(
 
 
 cleanup:
+    if (pCred)
+    {
+        NTLM_UNLOCK_MUTEX(bInLock, &pCred->Mutex);
+    }
+
     LW_SAFE_FREE_STRING(pWorkstation);
     LW_SAFE_FREE_STRING(pDomain);
+
     return dwError;
+
 error:
     LW_SAFE_FREE_MEMORY(pOutput->pvBuffer);
     pOutput->cbBuffer = 0;
     pOutput->BufferType = 0;
+
     // If this function has already succeed once, we MUST make sure phNewContext
     // is set so the caller can cleanup whatever context is remaining.  It
     // could be the original negotiate context or a new response context but

lsass/server/ntlm/setcreds.c
@@ -96,6 +96,8 @@ NtlmServerSetCredDomainNameAttribute(
 {
     DWORD dwError = LW_ERROR_SUCCESS;
     PNTLM_CREDENTIALS pNtlmCreds = (PNTLM_CREDENTIALS)hCred;
+    BOOLEAN bInLock = FALSE;
+    PSTR pName = NULL;
 
     if (!hCred || !pDomainName || !pDomainName->pName)
     {
@@ -103,14 +105,19 @@ NtlmServerSetCredDomainNameAttribute(
         BAIL_ON_LSA_ERROR(dwError);
     }
 
-    LW_SAFE_FREE_STRING(pNtlmCreds->pszDomainName);
+    NTLM_LOCK_MUTEX(bInLock, &pNtlmCreds->Mutex);
 
     dwError = LwAllocateString(
                   pDomainName->pName,
-                  &pNtlmCreds->pszDomainName);
+                  &pName);
     BAIL_ON_LSA_ERROR(dwError);
 
+    LW_SAFE_FREE_STRING(pNtlmCreds->pszDomainName);
+    pNtlmCreds->pszDomainName = pName;
+
 error:
+
+    NTLM_UNLOCK_MUTEX(bInLock, &pNtlmCreds->Mutex);
 
     return dwError;
 }

commit 1130e66f00570fc1733f4f9897f202d94ec20c8f

Fix for bug #12967.  Fix crash in lsass caused by r60191 due to not initializing the mutex.
CR:  dalmeida


git-svn-id: svn+ssh://source-control.beyondtrust.com/sources/repo/bt-dev/trunk/Platform/src/linux@60853 9fc1c11e-11b2-4e14-94d8-6c5e05775969

lsass/server/ntlm/credentials.c
@@ -98,6 +98,10 @@ NtlmCreateCredential(
     pCreds->nRefCount = 1;
     pCreds->dwCredDirection = dwDirection;
 
+    dwError = LwMapErrnoToLwError(pthread_mutex_init(&pCreds->Mutex, NULL));
+    BAIL_ON_LSA_ERROR(dwError);
+    pCreds->pMutex = &pCreds->Mutex;
+
 cleanup:
     *ppNtlmCreds = pCreds;
     return dwError;
@@ -173,6 +177,10 @@ NtlmFreeCredential(
 {
     LsaReleaseCredential(&pCreds->CredHandle);
     LW_SAFE_FREE_STRING(pCreds->pszDomainName);
+    if (pCreds->pMutex)
+    {
+        pthread_mutex_destroy(pCreds->pMutex);
+    }
     LW_SAFE_FREE_MEMORY(pCreds);
 }
 

lsass/server/ntlm/structs.h
@@ -258,6 +258,7 @@ typedef struct _NTLM_CREDENTIALS
     PSTR pszDomainName;
     LONG nRefCount;
     pthread_mutex_t Mutex;
+    pthread_mutex_t *pMutex;
 } NTLM_CREDENTIALS, *PNTLM_CREDENTIALS;
 
 typedef struct _NTLM_CONFIG