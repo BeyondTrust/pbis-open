061da24e872a2ffe7153fa2d73b1c76dbd315551
kstemen <kstemen@9fc1c11e-11b2-4e14-94d8-6c5e05775969>
2012-01-16 22:09:28 +0000

    Add some debugging inside cyrus sasl's bootstrap phase
    git-svn-id: svn+ssh://source-control.beyondtrust.com/sources/repo/bt-dev/trunk/Platform/src/linux@63062 9fc1c11e-11b2-4e14-94d8-6c5e05775969

--- cyrus-sasl/autogen.sh
+++ cyrus-sasl/autogen.sh
@@ -5,14 +5,54 @@
 rm -f aclocal.m4
 libtoolize -fic || exit $?
 aclocal --force -I cmulocal -I config || exit $?
+echo "cyrus after aclocal"
+if type stat >/dev/null 2>&1; then
+stat aclocal.m4
+stat config.h.in
+fi
 autoheader -f || exit $?
+echo "cyrus after autoheader"
+if type stat >/dev/null 2>&1; then
+stat aclocal.m4
+stat config.h.in
+fi
 autoconf -f || exit $?
+echo "cyrus after autoconf"
+if type stat >/dev/null 2>&1; then
+stat aclocal.m4
+stat config.h.in
+fi
 automake --add-missing --include-deps || exit $?
+echo "cyrus after automake"
+if type stat >/dev/null 2>&1; then
+stat aclocal.m4
+stat config.h.in
+fi
 
 cd saslauthd || exit $?
 rm -f aclocal.m4
 libtoolize -fic || exit $?
 aclocal --force -I ../cmulocal -I ../config || exit $?
+echo "cyrus after aclocal2"
+if type stat >/dev/null 2>&1; then
+stat aclocal.m4
+stat config.h.in
+fi
 autoheader -f || exit $?
+echo "cyrus after autoheader2"
+if type stat >/dev/null 2>&1; then
+stat aclocal.m4
+stat config.h.in
+fi
 autoconf -f || exit $?
+echo "cyrus after autoconf2"
+if type stat >/dev/null 2>&1; then
+stat aclocal.m4
+stat config.h.in
+fi
 automake --add-missing --include-deps || exit $?
+echo "cyrus after automake2"
+if type stat >/dev/null 2>&1; then
+stat aclocal.m4
+stat config.h.in
+fi
