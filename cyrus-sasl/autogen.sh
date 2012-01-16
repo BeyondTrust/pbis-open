8d0891837a863e0f97b8344ea68ecf0ac041932c
kstemen <kstemen@9fc1c11e-11b2-4e14-94d8-6c5e05775969>
2012-01-16 22:15:07 +0000

    Fix debug build log
    git-svn-id: svn+ssh://source-control.beyondtrust.com/sources/repo/bt-dev/trunk/Platform/src/linux@63063 9fc1c11e-11b2-4e14-94d8-6c5e05775969

--- cyrus-sasl/autogen.sh
+++ cyrus-sasl/autogen.sh
@@ -7,26 +7,26 @@ libtoolize -fic || exit $?
 aclocal --force -I cmulocal -I config || exit $?
 echo "cyrus after aclocal"
 if type stat >/dev/null 2>&1; then
-stat aclocal.m4
-stat config.h.in
+stat aclocal.m4 || true
+stat config.h.in || true
 fi
 autoheader -f || exit $?
 echo "cyrus after autoheader"
 if type stat >/dev/null 2>&1; then
-stat aclocal.m4
-stat config.h.in
+stat aclocal.m4 || true
+stat config.h.in || true
 fi
 autoconf -f || exit $?
 echo "cyrus after autoconf"
 if type stat >/dev/null 2>&1; then
-stat aclocal.m4
-stat config.h.in
+stat aclocal.m4 || true
+stat config.h.in || true
 fi
 automake --add-missing --include-deps || exit $?
 echo "cyrus after automake"
 if type stat >/dev/null 2>&1; then
-stat aclocal.m4
-stat config.h.in
+stat aclocal.m4 || true
+stat config.h.in || true
 fi
 
 cd saslauthd || exit $?
@@ -35,24 +35,24 @@ libtoolize -fic || exit $?
 aclocal --force -I ../cmulocal -I ../config || exit $?
 echo "cyrus after aclocal2"
 if type stat >/dev/null 2>&1; then
-stat aclocal.m4
-stat config.h.in
+stat aclocal.m4 || true
+stat config.h.in || true
 fi
 autoheader -f || exit $?
 echo "cyrus after autoheader2"
 if type stat >/dev/null 2>&1; then
-stat aclocal.m4
-stat config.h.in
+stat aclocal.m4 || true
+stat config.h.in || true
 fi
 autoconf -f || exit $?
 echo "cyrus after autoconf2"
 if type stat >/dev/null 2>&1; then
-stat aclocal.m4
-stat config.h.in
+stat aclocal.m4 || true
+stat config.h.in || true
 fi
 automake --add-missing --include-deps || exit $?
 echo "cyrus after automake2"
 if type stat >/dev/null 2>&1; then
-stat aclocal.m4
-stat config.h.in
+stat aclocal.m4 || true
+stat config.h.in || true
 fi
