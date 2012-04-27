5d202dfdf9d12ac1140a5f505ffd7109f6f25543
ssalley <ssalley@9fc1c11e-11b2-4e14-94d8-6c5e05775969>
2012-04-27 23:19:31 +0000

    Remove unused variables
    git-svn-id: svn+ssh://source-control.beyondtrust.com/sources/repo/bt-dev/trunk/Platform/src/linux@64312 9fc1c11e-11b2-4e14-94d8-6c5e05775969

--- eventlog/server/server.c
+++ eventlog/server/server.c
@@ -480,8 +480,6 @@ EVTRegisterEndpoint(
     volatile DWORD dwRpcStatus = 0;
     unsigned32 tmpStatus = 0;
     rpc_binding_vector_p_t pServerBinding = NULL;
-    BOOLEAN bEPRegistered = FALSE;
-    BOOLEAN bBound = TRUE;
 
     TRY
     {
@@ -498,7 +496,6 @@ EVTRegisterEndpoint(
     BAIL_ON_DCE_ERROR(dwError, dwRpcStatus);
     BAIL_ON_EVT_ERROR(dwError);
 
-    bBound = TRUE;
 
     TRY
     {
@@ -523,7 +520,6 @@ EVTRegisterEndpoint(
     BAIL_ON_DCE_ERROR(dwError, dwRpcStatus);
     BAIL_ON_EVT_ERROR(dwError);
 
-    bEPRegistered = TRUE;
     EVT_LOG_INFO("RPC Endpoint registered successfully.");
 
 cleanup:
