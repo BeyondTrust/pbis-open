e0020a62ddb4f16c11b9d8aed0d60f0c6abc20ef
rali <rali@9fc1c11e-11b2-4e14-94d8-6c5e05775969>
2013-05-07 18:05:58 +0000

    look for sendtrap in /lib64
    git-svn-id: svn+ssh://source-control.beyondtrust.com/sources/repo/bt-dev/trunk/Platform/src/linux@65578 9fc1c11e-11b2-4e14-94d8-6c5e05775969

--- eventlog/server/sendtrap_stub.c
+++ eventlog/server/sendtrap_stub.c
@@ -37,7 +37,8 @@
 #include "includes.h"
 // #include <dlfcn.h>
 
-#define SENDTRAP_DLL_NAME  "/opt/pbis/lib/sendtrap/sendtrap.so"
+#define SENDTRAP_DLL_NAME      "/opt/pbis/lib/sendtrap/sendtrap.so"
+#define SENDTRAP_DLL_NAME_64   "/opt/pbis/lib64/sendtrap/sendtrap.so"
 
 typedef VOID (*PFN_SendTrapSetup) (void);
 typedef VOID (*PFN_SendTrapTearDown) ();
@@ -66,11 +67,15 @@ DWORD EvtSnmpSetup()
   gbSendTrapLibInitialized = TRUE;
 
    // open library dlopen(sendtrap.so) 
-  gpSendTrapLibHandle = dlopen(SENDTRAP_DLL_NAME, RTLD_LAZY);
+  gpSendTrapLibHandle = dlopen(SENDTRAP_DLL_NAME_64, RTLD_LAZY);
   if (gpSendTrapLibHandle == NULL)
   {
-      dwError = LW_ERROR_LOAD_LIBRARY_FAILED;
-      BAIL_ON_EVT_ERROR(dwError);
+      gpSendTrapLibHandle = dlopen(SENDTRAP_DLL_NAME, RTLD_LAZY);
+      if (gpSendTrapLibHandle == NULL)
+      {
+         dwError = LW_ERROR_LOAD_LIBRARY_FAILED;
+         BAIL_ON_EVT_ERROR(dwError);
+      }
   }
 
   gpfnSendTrapSetup = (PFN_SendTrapSetup) dlsym(gpSendTrapLibHandle, "SendTrapSnmpSetup");
