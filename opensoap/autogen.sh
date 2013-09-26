be24e95350dc0d8652c9be10503a51d768a31f26
alok.raj <alok.raj@9fc1c11e-11b2-4e14-94d8-6c5e05775969>
2013-09-26 07:08:30 +0000

    autoenroll: temporary changes for building error in Platform, changes in opensoap/autogen.sh
    git-svn-id: svn+ssh://source-control.beyondtrust.com/sources/repo/bt-dev/trunk/Platform/src/linux@66307 9fc1c11e-11b2-4e14-94d8-6c5e05775969

--- opensoap/autogen.sh
+++ opensoap/autogen.sh
@@ -1,11 +1,12 @@
 #! /bin/sh
+set -x
 rm -rf autom4te.cache/;
 rm -f aclocal.m4;
-set -x
 if test ! -d config; then mkdir config; fi
-aclocal  --force -I config || exit $?
+#aclocal  --force -I config || exit $?
 #libtoolize --force --copy
 libtoolize -fic || exit $?
+aclocal  --force || exit $?
 autoheader
 automake --foreign --add-missing --copy
 autoconf
