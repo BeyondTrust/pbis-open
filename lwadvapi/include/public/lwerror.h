ef7a5b348b505a4172d80b7546789aee31ce18f6
rseto <rseto@9fc1c11e-11b2-4e14-94d8-6c5e05775969>
2014-07-03 19:13:42 +0000

    missed file in last commit
    git-svn-id: svn+ssh://source-control.beyondtrust.com/sources/repo/bt-dev/trunk/Platform/src/linux@67953 9fc1c11e-11b2-4e14-94d8-6c5e05775969

--- lwadvapi/include/public/lwerror.h
+++ lwadvapi/include/public/lwerror.h
@@ -385,6 +385,8 @@
 #define LW_ERROR_LDAP_TOO_LATE                             40351
 #define LW_ERROR_LDAP_CANNOT_CANCEL                        40352
 #define LW_ERROR_LDAP_ASSERTION_FAILED                     40353
+#define LW_ERROR_LDAP_CONSTRAINT_VIOLATION_SPN             40354
+
 
 #define LW_ERROR_UID_TOO_LOW                               40400
 #define LW_ERROR_UID_TOO_HIGH                              40401
