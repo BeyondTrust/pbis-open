cfae6c317051c409e83016e1ebfa7d88af0b324c
rauch <rauch@9fc1c11e-11b2-4e14-94d8-6c5e05775969>
2015-09-09 21:18:34 +0000

    Add TrustEnumerationWait defaults back into product, since they were reintroduced in PBIS 7.5.  This requires duplicating getdnsdomain() from reg61sed.sh
    git-svn-id: svn+ssh://source-control.beyondtrust.com/sources/repo/bt-dev/trunk/Platform/src/linux@69197 9fc1c11e-11b2-4e14-94d8-6c5e05775969

--- lwupgrade/tools/reg2schema/regupgr61.sh
+++ lwupgrade/tools/reg2schema/regupgr61.sh
@@ -4,6 +4,15 @@ BIN_DIR=/opt/pbis/bin
 LWREGSHELL=$BIN_DIR/regshell
 PSTORE_UPGRADE=/opt/pbis/libexec/reg61sed.sh
 
+get_dns_domain()
+{
+  infile=$1
+  grep '^"DomainDnsName"=' $infile | \
+       sed -e 's|DomainDnsName.*=||' -e 's|"||g' | \
+       tr -d '\r'
+}
+
+
 if [ "$1" != "--install" ]; then
   echo "WARNING: This tool is called during system installation"
   echo "         and upgrade, and should not be called by an end-user."
@@ -35,6 +44,15 @@ fi
 # Import renamed pstore entries
 $LWREGSHELL import ${tmpreg}.out
 rm -f ${tmpreg}.out
+
+
+# Need to add TrustEnumerationWait commands if they don't exist - there should be 2 settings
+DOMAIN_NAME=`get_dns_domain ${tmpreg} | tr '[a-z]' '[A-Z]'`
+TRUSTWAITCOUNT=`grep -c TrustEnumerationWait ${tmpreg}`
+if [ $TRUSTWAITCOUNT -lt 2 ]; then
+    $LWREGSHELL  add_value '[HKEY_THIS_MACHINE\Services\lsass\Parameters\Providers\ActiveDirectory\DomainJoin\'$DOMAIN_NAME']' TrustEnumerationWait REG_DWORD 0
+    $LWREGSHELL  add_value '[HKEY_THIS_MACHINE\Services\lsass\Parameters\Providers\ActiveDirectory\DomainJoin\'$DOMAIN_NAME']' TrustEnumerationWaitSeconds REG_DWORD 0
+fi
 
 # Clear out old pstore entries
 # Using "." to avoid quadruple backslashes
