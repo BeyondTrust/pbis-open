5ca25147ecb32dc3e512b267822d5ff7e90357b0
mdrouin <mdrouin@mdrouin-batcave-debian.bt1.com>
2016-05-20 11:47:36 -0300

    Added string to indicate to user that we're leaving the domain #TFS53628

--- domainjoin/domainjoin-cli/src/main.c
+++ domainjoin/domainjoin-cli/src/main.c
@@ -591,7 +591,10 @@ void DoLeaveNew(int argc, char **argv, int columns, BOOLEAN isEnterprise, LWExce
         else if(!strcmp(argv[0], "--preview"))
             preview = TRUE;
         else if(!strcmp(argv[0], "--releaseLicense") && isEnterprise)
+        {
             options.releaseLicense = TRUE; 
+            fprintf(stdout, "License release request sent.\n");
+        }
         // remaining options require at least two options 
         else if(argc < 2)
         {
