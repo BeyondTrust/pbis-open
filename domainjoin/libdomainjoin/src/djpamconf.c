fa53ea432ff906e8fd0d85ac8907bc5fd8abde6b
rali <rali>
2016-08-12 14:46:27 -0400

    Configure single entry for pam_lsass in password and account phase

--- domainjoin/libdomainjoin/src/djpamconf.c
+++ domainjoin/libdomainjoin/src/djpamconf.c
@@ -1946,6 +1946,22 @@ static BOOLEAN PamModuleRemoveOnEnable( const char * phase, const char *control,
         return TRUE;
     }
 
+    // RALI
+    if (!strcmp(phase, "auth") && !strcmp(control, "requisite") && !strcmp(buffer, "pam_lwidentity_smartcard_prompt"))
+    {
+        DJ_LOG_ERROR("RALI PamModuleRemoveOnEnable  Looking at entry %s %s %s", phase, control, module);
+        return TRUE;
+    }
+
+    if (!strcmp(phase, "account") && !strcmp(control, "required"))
+        DJ_LOG_ERROR("RALII PamModuleRemoveOnEnable  Looking at entry %s %s %s %s", phase, control, module, buffer);
+
+    if (!strcmp(phase, "account") && !strcmp(control, "required") && !strcmp(buffer, "pam_lsass"))
+    {
+        DJ_LOG_ERROR("RALIIII PamModuleRemoveOnEnable  Looking at entry %s %s %s", phase, control, module);
+        return TRUE;
+    }
+
     return FALSE;
 }
 
@@ -2013,6 +2029,10 @@ static BOOLEAN PamModuleAlwaysDeniesDomainLogins( const char * phase, const char
         return TRUE;
     if(!strcmp(buffer, "pam_cracklib"))
         return TRUE;
+
+    if (!strcmp(buffer, "pam_faillock"))
+        return TRUE;
+
     if(!strcmp(buffer, "pam_pwcheck"))
         return TRUE;
 
@@ -2226,7 +2246,8 @@ static DWORD PamOldCenterisDisable(struct PamConf *conf, const char *service, co
         lineObj = &conf->lines[line];
         GetModuleControl(lineObj, &module, &control);
 
-        DJ_LOG_VERBOSE("Looking at entry %s %s %s %s", service, phase, control, module);
+        // DJ_LOG_VERBOSE("Looking at entry %s %s %s %s", service, phase, control, module);
+        DJ_LOG_ERROR("RALI PamOldCenterisDisable Looking at entry %s %s %s %s", service, phase, control, module);
 
         CT_SAFE_FREE_STRING(includeService);
         ceError = GetIncludeName(
@@ -2340,7 +2361,8 @@ static DWORD PamLwidentityDisable(struct PamConf *conf, const char *service, con
         lineObj = &conf->lines[line];
         GetModuleControl(lineObj, &module, &control);
 
-        DJ_LOG_VERBOSE("Looking at entry %s %s %s %s", service, phase, control, module);
+        // DJ_LOG_VERBOSE("Looking at entry %s %s %s %s", service, phase, control, module);
+        DJ_LOG_ERROR("RALI PamLwidentityDisable  Looking at entry %s %s %s %s", service, phase, control, module);
 
         CT_SAFE_FREE_STRING(includeService);
         ceError = GetIncludeName(
@@ -2523,7 +2545,8 @@ static void FixPromptingModule(struct PamConf *conf, const char *service, const 
         lineObj = &conf->lines[line];
         GetModuleControl(lineObj, &module, &control);
 
-        DJ_LOG_VERBOSE("Looking at entry %s %s %s %s", service, phase, control, module);
+        // DJ_LOG_VERBOSE("Looking at entry %s %s %s %s", service, phase, control, module);
+        DJ_LOG_ERROR("RALI FixPromptingModule Looking at entry %s %s %s %s", service, phase, control, module);
         if(PamModulePrompts(phase, module))
         {
             DJ_LOG_INFO("Making sure module %s uses the password stored by pam_lwidentity", module);
@@ -2534,6 +2557,7 @@ static void FixPromptingModule(struct PamConf *conf, const char *service, const 
                     const char *optionName = "try_first_pass";
                     if(!PamModuleUnderstandsTryFirstPass(phase, module))
                         optionName = "use_first_pass";
+
                     LW_CLEANUP_CTERR(exc, AddOption(conf, line, optionName));
                 }
             }
@@ -2679,7 +2703,8 @@ static void PamLwidentityEnable(const char *testPrefix, const LwDistroInfo *dist
         lineObj = &conf->lines[line];
         GetModuleControl(lineObj, &module, &control);
 
-        DJ_LOG_VERBOSE("Looking at entry %s %s %s %s", service, phase, control, module);
+        // DJ_LOG_VERBOSE("Looking at entry %s %s %s %s", service, phase, control, module);
+        DJ_LOG_ERROR("RALI PamLwidentityEnable Looking at entry %s %s %s %s", service, phase, control, module);
 
         CT_SAFE_FREE_STRING(includeService);
         ceError = GetIncludeName(
@@ -2803,6 +2828,7 @@ static void PamLwidentityEnable(const char *testPrefix, const LwDistroInfo *dist
         if(!strcmp(module, "pam_lwidentity_smartcard_prompt"))
             state->hasAddedSmartCardPrompt = TRUE;
 
+/* RALI 
         if(!state->hasAddedSmartCardPrompt && !strcmp(phase, "auth") &&
                 PamModulePrompts(phase, module))
         {
@@ -2818,6 +2844,7 @@ static void PamLwidentityEnable(const char *testPrefix, const LwDistroInfo *dist
             GetModuleControl(lineObj, &module, &control);
             LW_TRY(exc, FixPromptingModule(conf, service, phase, state, line, &LW_EXC));
         }
+RALI */ 
 
         if(PamModuleAlwaysDeniesDomainLogins(phase, module, distro) && (
                     !strcmp(control, "required") ||
@@ -3167,6 +3194,7 @@ static void PamLwidentityEnable(const char *testPrefix, const LwDistroInfo *dist
             state->sawPromptingModule = TRUE;
         }
 
+
         /* Fill in the correct values for lwidentityLine */
         lineObj = &conf->lines[lwidentityLine];
         /* This is incase the phase is @include */
@@ -3197,10 +3225,14 @@ static void PamLwidentityEnable(const char *testPrefix, const LwDistroInfo *dist
             * account required pam_lwidentity unknown_ok
             * account sufficient pam_lwidentity
             */
-            LW_CLEANUP_CTERR(exc, CopyLineAndUpdateSkips(conf, lwidentityLine, NULL));
-            lineObj = &conf->lines[lwidentityLine];
-            LW_CLEANUP_CTERR(exc, SetPamTokenValue(&lineObj->control, lineObj->phase, "required"));
+        //    LW_CLEANUP_CTERR(exc, CopyLineAndUpdateSkips(conf, lwidentityLine, NULL));
+        //    lineObj = &conf->lines[lwidentityLine];
+        //    LW_CLEANUP_CTERR(exc, SetPamTokenValue(&lineObj->control, lineObj->phase, "required"));
+        //    LW_CLEANUP_CTERR(exc, AddOption(conf, lwidentityLine, "unknown_ok"));
+        //
+            DJ_LOG_ERROR("RALI PamLwidentityEnable adding unknown option");
             LW_CLEANUP_CTERR(exc, AddOption(conf, lwidentityLine, "unknown_ok"));
+             
         }
         else if(state->sawPromptingModule)
         {
@@ -3310,7 +3342,8 @@ static DWORD PamLwiPassPolicyDisable(struct PamConf *conf, const char *service, 
         lineObj = &conf->lines[line];
         GetModuleControl(lineObj, &module, &control);
 
-        DJ_LOG_VERBOSE("Looking at entry %s %s %s %s", service, phase, control, module);
+        // DJ_LOG_VERBOSE("Looking at entry %s %s %s %s", service, phase, control, module);
+        DJ_LOG_ERROR("RALI PamLwiPassPolicyDisable Looking at entry %s %s %s %s", service, phase, control, module);
 
         CT_SAFE_FREE_STRING(includeService);
         ceError = GetIncludeName(
@@ -3400,7 +3433,8 @@ static DWORD PamLwiPassPolicyEnable(struct PamConf *conf, const char *service, c
         lineObj = &conf->lines[line];
         GetModuleControl(lineObj, &module, &control);
 
-        DJ_LOG_VERBOSE("Looking at entry %s %s %s %s", service, phase, control, module);
+        // DJ_LOG_VERBOSE("Looking at entry %s %s %s %s", service, phase, control, module);
+        DJ_LOG_ERROR("RALI PamLwiPassPolicyEnable Looking at entry %s %s %s %s", service, phase, control, module);
 
         CT_SAFE_FREE_STRING(includeService);
         ceError = GetIncludeName(
