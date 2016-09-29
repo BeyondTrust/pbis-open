0d9e86af1d8e921b188f7aa6f802168326b19f66
rali <rali>
2016-09-29 15:30:55 -0400

    #14178 - Search user and exclude computer object class

--- lwadtool/libadtool/search.c
+++ lwadtool/libadtool/search.c
@@ -258,14 +258,14 @@ DWORD ExecuteAdtSearchUserAction(IN AdtActionTP action)
     else {
         if (IsNVP(action->searchUser.name)) {
             dwError = LwAllocateStringPrintf(&filter,
-                                             "(&(objectClass=user)(%s))",
+                                             "(&(objectClass=user)(!(objectClass=computer))(%s))",
                                              action->searchUser.name);
             ADT_BAIL_ON_ALLOC_FAILURE_NP(!dwError);
         }
         else {
             dwError
                     = LwAllocateStringPrintf(&filter,
-                                             "(&(objectClass=user)(|(%s=%s)(CN=%s)))",
+                                             "(&(objectClass=user)(!(objectClass=computer))(|(%s=%s)(CN=%s)))",
                                              IsUPN(action->searchUser.name)
                                                      ? "userPrincipalName"
                                                      : "sAMAccountName",
