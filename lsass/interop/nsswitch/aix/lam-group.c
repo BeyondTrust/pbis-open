7723622c2d35b9d2090f7db37ea3141ed1922323
Richard Boulton <rboulton@beyondtrust.com>
2017-06-16 12:47:54 -0300

    Merged PR 1259: Merge TFS88248-AIX-LWSMD-SIZE to master
    Related work items: #88248

--- lsass/interop/nsswitch/aix/lam-group.c
+++ lsass/interop/nsswitch/aix/lam-group.c
@@ -31,6 +31,8 @@
 #include "includes.h"
 #include "lam-group.h"
 
+static const DWORD MAX_NUM_GROUPS = 500;
+
 void
 LsaNssFreeLastGroup(
         VOID
@@ -481,15 +483,26 @@ LsaNssListGroups(
 {
     DWORD dwError = LW_ERROR_SUCCESS;
     const DWORD dwInfoLevel = 0;
-    const DWORD dwEnumLimit = 100000;
+    const DWORD dwEnumLimit = MAX_NUM_GROUPS;
     DWORD dwIndex = 0;
+    DWORD dwTotalIndex = 0;
     DWORD dwGroupsFound = 0;
     HANDLE hResume = (HANDLE)NULL;
-    size_t sRequiredMem = 0;
+    size_t sRequiredMem = 1;
+    size_t sUsedMem = 0;
     PLSA_GROUP_INFO_0* ppGroupList = NULL;
     PSTR pszListStart = NULL;
+    PSTR pDisabled = getenv(DISABLE_NSS_ENUMERATION_ENV);
     // Do not free
     PSTR pszPos = NULL;
+
+    LSA_LOG_PAM_DEBUG("Enumerating groups");
+
+    if (pDisabled) 
+    {
+        pResult->attr_flag = ENOENT;
+        goto error;
+    }
 
     dwError = LsaBeginEnumGroups(
                 hLsaConnection,
@@ -499,47 +512,64 @@ LsaNssListGroups(
                 &hResume);
     BAIL_ON_LSA_ERROR(dwError);
 
-    dwError = LsaEnumGroups(
-                hLsaConnection,
-                hResume,
-                &dwGroupsFound,
-                (PVOID**)&ppGroupList);
-    BAIL_ON_LSA_ERROR(dwError);
+    do {
+        if (ppGroupList != NULL)
+        {
+            LsaFreeGroupInfoList(
+                    dwInfoLevel,
+                    (PVOID*)ppGroupList,
+                    dwGroupsFound);
 
-    if (dwGroupsFound == dwEnumLimit)
-    {
-        pResult->attr_flag = ENOMEM;
-        goto error;
-    }
+            ppGroupList = NULL;
+            dwGroupsFound = 0;
+        }
 
-    sRequiredMem = 1;
-    for (dwIndex = 0; dwIndex < dwGroupsFound; dwIndex++)
-    {
-        sRequiredMem += strlen(ppGroupList[dwIndex]->pszName) + 1;
-    }
-    if (dwIndex == 0)
-    {
-        sRequiredMem++;
-    }
+        dwError = LsaEnumGroups(
+                    hLsaConnection,
+                    hResume,
+                    &dwGroupsFound,
+                    (PVOID**)&ppGroupList);
+        BAIL_ON_LSA_ERROR(dwError);
 
-    dwError = LwAllocateMemory(
-                sRequiredMem,
-                (PVOID*)&pszListStart);
-    BAIL_ON_LSA_ERROR(dwError);
-    pszPos = pszListStart;
+        if (dwGroupsFound == 0) continue;
 
-    for (dwIndex = 0; dwIndex < dwGroupsFound; dwIndex++)
-    {
-        strcpy(pszPos, ppGroupList[dwIndex]->pszName);
-        pszPos += strlen(pszPos) + 1;
-    }
-    if (dwIndex == 0)
-    {
+        LSA_LOG_PAM_DEBUG("Found %u groups", (unsigned int)dwGroupsFound);
+
+        for (dwIndex = 0; dwIndex < dwGroupsFound; dwIndex++)
+        {
+            if (ppGroupList[dwIndex]->pszName) 
+            {
+                sRequiredMem += strlen(ppGroupList[dwIndex]->pszName) + 1;
+            }
+        }
+
+        dwError = LwReallocMemory(
+                    pszListStart,
+                    (PVOID*)&pszListStart,
+                    sRequiredMem);
+        BAIL_ON_LSA_ERROR(dwError);
+
+        pszPos = pszListStart + sUsedMem;
+
+        for (dwIndex = 0; dwIndex < dwGroupsFound; dwIndex++)
+        {
+            if (ppGroupList[dwIndex]->pszName) 
+            {
+                strcpy(pszPos, ppGroupList[dwIndex]->pszName);
+                pszPos += strlen(pszPos) + 1;
+            }
+        }
+        
+        sUsedMem = pszPos - pszListStart;
+        
+        dwTotalIndex += dwGroupsFound;
+
+    } while (dwGroupsFound > 0);
+    
+    if (pszPos) {
         *pszPos++ = 0;
+        assert(pszPos == pszListStart + sRequiredMem);
     }
-    *pszPos++ = 0;
-
-    assert(pszPos == pszListStart + sRequiredMem);
 
     pResult->attr_un.au_char = pszListStart;
     pResult->attr_flag = 0;
